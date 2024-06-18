5ec211ab5dc7d4e18dca3a27774915503ce376b2
sschildhauer <sschildhauer@ucdavis.edu>
2024-06-18 13:00:33 -0700

    Updating code to reflect updates from main

--- data-raw/v9na.submit_longdata_SS.r
+++ data-raw/v9na.submit_longdata_SS.r
@@ -1,7 +1,8 @@
 
 devtools::load_all()
 
-
+devtools::install_github("ucd-serg/serocalculator")
+library(serocalculator)
 library(tidyverse)
 library(runjags)
 library(coda)
@@ -40,20 +41,22 @@ dL <-
   mutate(visit_num = rank(visit, ties.method = "first")) %>%
   ungroup()
 
-
+#set seed for reproducibility
+set.seed(54321)
 #subset data for checking
-# dL_sub <- dL %>%
-#   filter(index_id %in% sample(unique(index_id), 20))
+dL_sub <- dL %>%
+  filter(index_id %in% sample(unique(index_id), 50))
 
 #Filtering data before running through program
-dL_young <- dL %>% filter(age_years <= 5)
-dL_old <- dL %>% filter(age_years > 5)
-dL_vaccine <- dL %>% filter(cohort == "Vaccinee")
-dL_case <- dL %>% filter(cohort == "Case")
+# dL_young <- dL %>% filter(age_years <= 5)
+# dL_old <- dL %>% filter(age_years > 5)
+# dL_vaccine <- dL %>% filter(cohort == "Vaccinee")
+# dL_case <- dL %>% filter(cohort == "Case")
+# dL_sub <- dL
 
-dL_sub <- dL
 #prepare data for modeline
 longdata <- prep_data(dL_sub)
+priors <- prep_priors(max_antigens = longdata$n_antigen_isos)
 
 
 #inputs for jags model
@@ -80,7 +83,7 @@ initsfunction <- function(chain){
 }
 
 
-jags.post <- run.jags(model=file.mod,data=longdata,
+jags.post <- run.jags(model=file.mod,data=c(longdata, priors),
                       inits=initsfunction,method="parallel",
                       adapt=nadapt,burnin=nburnin,thin=nthin,sample=nmc,
                       n.chains=nchains,
@@ -91,7 +94,6 @@ jags.post <- run.jags(model=file.mod,data=longdata,
 # jags.post_old <- jags.post
 # jags.post_vaccine <- jags.post
 # jags.post_case <- jags.post
-
 
 ### SS Diagonstic code 
 
@@ -144,40 +146,64 @@ summ_jags("[3,3]")
 
 # Look into these lines. Want to be able to work with output in ggplot. 
 # ggmcmc package 
+# mcmc_list <- as.mcmc.list(jags.post)
+# mcmc_matrix <- as.matrix(mcmc_list)
+# mcmc_df <- as.data.frame(mcmc_matrix)
 mcmc_list <- as.mcmc.list(jags.post)
-mcmc_matrix <- as.matrix(mcmc_list)
-mcmc_df <- as.data.frame(mcmc_matrix)
+mcmc_df <- ggs(mcmc_list)
   
-# Adding iteration numbers
-iterations <- rep(1:nrow(mcmc_matrix), each = ncol(mcmc_matrix))
+# # Adding iteration numbers
+# iterations <- rep(1:nrow(mcmc_matrix), each = ncol(mcmc_matrix))
+# 
+# # Reshape the data frame
+# long_predpar_df <- pivot_longer(mcmc_df, cols = everything(), names_to = "Variable", values_to = "value")
+# long_predpar_df$iter <- iterations
 
-# Reshape the data frame
-long_predpar_df <- pivot_longer(mcmc_df, cols = everything(), names_to = "Variable", values_to = "value")
-long_predpar_df$iter <- iterations
+
+# wide_predpar_df <- long_predpar_df %>%
+#   mutate(
+#     index_id = as.numeric(sub("^par\\[(\\d+),.*", "\\1", Variable)),
+#     antigen_iso = as.numeric(sub("^par\\[\\d+,(\\d+),.*", "\\1", Variable)),
+#     parameter = as.numeric(sub("^par\\[\\d+,\\d+,(\\d+)\\]", "\\1", Variable))
+#   ) %>%
+#   mutate(
+#     index_id = factor(index_id, labels = c(unique(dL_sub$index_id), "newperson")),  
+#     antigen_iso = factor(antigen_iso, labels = unique(dL_sub$antigen_iso)), 
+#                          # parnum: use y0=1; y1=2; t1=3; alpha=4; shape=5
+#                          # note to self - i dont like that these are not named anywhere....
+#       parameter = factor(parameter, labels = c("y0", "y1", "t1", "alpha", "r"))) %>%
+#       mutate(value = exp(value)) %>%
+#       mutate(value = ifelse(parameter == "r", value+1, value)) %>%
+#       ## only take the last subject (newperson)
+#       filter(index_id == "newperson") %>%
+#       select(-Variable) %>%
+#       pivot_wider(names_from = "parameter", values_from="value") %>%
+#       rowwise() %>%
+#       mutate(y1 = y0+y1) %>%
+#       droplevels() %>%
+#       ungroup()
 
 
-wide_predpar_df <- long_predpar_df %>%
+wide_predpar_df <- mcmc_df %>%
   mutate(
-    index_id = as.numeric(sub("^par\\[(\\d+),.*", "\\1", Variable)),
-    antigen_iso = as.numeric(sub("^par\\[\\d+,(\\d+),.*", "\\1", Variable)),
-    parameter = as.numeric(sub("^par\\[\\d+,\\d+,(\\d+)\\]", "\\1", Variable))
+    parameter = sub("^(\\w+)\\[.*", "\\1", Parameter),
+    index_id = as.numeric(sub("^\\w+\\[(\\d+),.*", "\\1", Parameter)),
+    antigen_iso = as.numeric(sub("^\\w+\\[\\d+,(\\d+).*", "\\1", Parameter))
   ) %>%
   mutate(
-    index_id = factor(index_id, labels = c(unique(dL_sub$index_id), "newperson")),  
-    antigen_iso = factor(antigen_iso, labels = unique(dL_sub$antigen_iso)), 
-                         # parnum: use y0=1; y1=2; t1=3; alpha=4; shape=5
-                         # note to self - i dont like that these are not named anywhere....
-      parameter = factor(parameter, labels = c("y0", "y1", "t1", "alpha", "r"))) %>%
-      mutate(value = exp(value)) %>%
-      mutate(value = ifelse(parameter == "r", value+1, value)) %>%
-      ## only take the last subject (newperson)
-      filter(index_id == "newperson") %>%
-      select(-Variable) %>%
-      pivot_wider(names_from = "parameter", values_from="value") %>%
-      rowwise() %>%
-      mutate(y1 = y0+y1) %>%
-      droplevels() %>%
-      ungroup()
+    index_id = factor(index_id, labels = attr(longdata, "ids")),
+    antigen_iso = factor(antigen_iso, labels = attr(longdata, "antigens"))) %>%
+  # mutate(value = exp(value)) %>%
+  # mutate(value = ifelse(parameter == "r", value+1, value)) %>%
+  ## only take the last subject (newperson)
+  filter(index_id == "newperson") %>%
+  select(-Parameter) %>%
+  pivot_wider(names_from = "parameter", values_from="value") %>%
+  rowwise() %>%
+  #mutate(y1 = y0+y1) %>%
+  droplevels() %>%
+  ungroup() %>%
+  rename(r = shape)
 
 
 
@@ -188,20 +214,14 @@ wide_predpar_df <- long_predpar_df %>%
 # devtools::install_github("ucd-serg/serocalculator")
 library(serocalculator)
 
-
-
-curve_params <- 
+curve_params <-
   wide_predpar_df
-  
-  class(curve_params) =
+class(curve_params) =
   c("curve_params", class(curve_params))
-  
-  antigen_isos = unique(curve_params$antigen_iso)
-  
-  attr(curve_params, "antigen_isos") = antigen_isos
-
-
+antigen_isos = unique(curve_params$antigen_iso)
+attr(curve_params, "antigen_isos") = antigen_isos
 autoplot(curve_params)
+
 # curve_params_young <- curve_params
 # curve_params_old <- curve_params
 # curve_params_vaccine <- curve_params
