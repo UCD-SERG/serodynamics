f5f1e9f9420495ba7052b9ef42cc81ac90ce76de
sschildhauer <sschildhauer@ucdavis.edu>
2024-08-12 10:35:13 +0700

    Comitting diagnostnic functions to submit_longdata_SS

--- data-raw/v9na.submit_longdata_SS_mcmc.r
+++ data-raw/v9na.submit_longdata_SS_mcmc.r
@@ -7,6 +7,8 @@ library(tidyverse)
 library(runjags)
 library(coda)
 library(ggmcmc)
+library(ggbeeswarm)
+library(ggpubr)
 
 #model file
 file.mod <- here::here()  %>% fs::path("inst/extdata/model.jags.r")
@@ -34,40 +36,29 @@ file.mod <- here::here()  %>% fs::path("inst/extdata/model.jags.r")
 
 #long data - CHOLERA
 dL <-
-  #read.csv("~/Library/CloudStorage/OneDrive-UniversityofCalifornia,Davis/Research/Cholera-longitudinal/data/cholera_data_compiled_050324.csv") %>%
+  # read.csv("~/Library/CloudStorage/OneDrive-UniversityofCalifornia,Davis/Research/Cholera-longitudinal/data/cholera_data_compiled_050324.csv") %>%
   read_csv(here::here()  %>% fs::path("inst/extdata/cholera_data_compiled_050324.csv")) %>%
   group_by(index_id, antigen_iso) %>%                      # Group data by individual
   arrange(visit) %>%                          # Sort data by visit within each group
   mutate(visit_num = rank(visit, ties.method = "first")) %>%
   ungroup()
 
-
-#Filtering data before running through program
-dL_young <- dL %>% filter(age_years <= 5)
-dL_old <- dL %>% filter(age_years > 5)
-dL_vaccine <- dL %>% filter(cohort == "Vaccinee")
-dL_case <- dL %>% filter(cohort == "Case")
-dL_sub <- dL_case
-
-
 #set seed for reproducibility
 set.seed(54321)
 #subset data for checking
-# dL_sub <- dL %>%
-#   filter(index_id %in% sample(unique(index_id), 50))
-
+dL_sub <- dL %>%
+  filter(index_id %in% sample(unique(index_id), 50))
 
 #prepare data for modeline
 longdata <- prep_data(dL_sub)
 priors <- prep_priors(max_antigens = longdata$n_antigen_isos)
-
 
 #inputs for jags model
 nchains <- 4;                # nr of MC chains to run simultaneously
 nadapt  <- 100;             # nr of iterations for adaptation
 nburnin <- 100;            # nr of iterations to use for burn-in
 nmc     <- 100;             # nr of samples in posterior chains
-niter   <- 10000;            # nr of iterations for posterior sample
+niter   <- 1000;            # nr of iterations for posterior sample
 nthin   <- round(niter/nmc); # thinning needed to produce nmc from niter
 
 #pred.subj <- longdata$nsubj + 1;
@@ -91,15 +82,8 @@ jags.post <- run.jags(model=file.mod,data=c(longdata, priors),
                       n.chains=nchains,
                       monitor=tomonitor,summarise=FALSE);
 
-
-
-
-
 mcmc_list <- as.mcmc.list(jags.post)
-
 mcmc_df <- ggs(mcmc_list)
-
-
 
 
 wide_predpar_df <- mcmc_df %>%
@@ -166,145 +150,79 @@ curve_params <-
 
   attr(curve_params, "antigen_isos") = antigen_isos
 
-
-autoplot(curve_params)
-curve_params_young <- curve_params
-curve_params_old <- curve_params
-curve_params_vaccine <- curve_params
-curve_params_case <- curve_params
-curve_params_immunity <- rbind(curve_params_vaccine %>% mutate(status="vaccinated")
-                               , curve_params_case %>% mutate(status="case"))
-autoplot(curve_params_immunity, group=status)
-
-
-#################################### SS Diagonstic code 
+#################################### SS diagonstic code begin
 # -- Diagnostics using ggmcmc package
 # -- Compiling into dataframe using ggs()
-visualize_jags_young <- ggs(jags.post$mcmc)
-visualize_jags_old <- ggs(jags.post$mcmc)
-visualize_jags_vaccine <- ggs(jags.post$mcmc)
-visualize_jags_case <- ggs(jags.post$mcmc)
+visualize_jags <- ggs(jags.post$mcmc)
 
-# Matching iso type to the mcmc output data frame
-iso_dat <- data.frame(attributes(longdata)$antigens) %>%
-  mutate(subnum = row.names(iso_dat))
+# Creating columns to subset the outputs -- using regular expressions to do so from the output
+iso_dat <- data.frame(attributes(longdata)$antigens) 
+iso_dat <- iso_dat %>% mutate(Subnum = row.names(iso_dat))
 visualize_jags <- visualize_jags %>%
   mutate(Subnum = sub('.*,','',Parameter),
          Parameter_sub = sub('\\[.*','',Parameter),
          Subject = sub('\\,.*','',Parameter)) %>%
-  mutate(Subnum = as.numeric(sub("\\].*",'',subnum)),
-         Subject = sub('.*[','',Parameter))
-# Merging iso dat in 
+  mutate(Subnum = as.numeric(sub("\\].*",'',Subnum)),
+         Subject = sub(".*\\[",'',Subject))
+
+# Merging iso dat in to ensure we are plotting the name of the antigen correctly
 visualize_jags <- merge(visualize_jags, iso_dat, "Subnum", all=T) 
 visualize_jags <- visualize_jags %>%
   rename(c("Iso_type"="attributes.longdata..antigens"))%>%
   select(!c("Subnum"))
-
-# Setting subset for the "new person"
-np <- longdata$nsubj
+# Setting subset for the "new person" -- setting it to the final sample
+np <- as.character(longdata$nsubj)
 visualize_jags_sub <- visualize_jags %>%
-  filter(grepl(paste0(np), Parameter))
+  filter(Subject == np)
+#Creating a label
+visualize_jags <- visualize_jags %>%
+  mutate(Label = paste0(Iso_type,", ",Parameter_sub)) 
 
-### Writing functions with antigen type as the input
-# Setting subset for the antigen type 
-iso <- function(iso) {
-  match(iso, attributes(longdata)$antigens)
-}
-
-### Preparing data set 
-
-
-plot_jags_hist <- function(x) {
-  #Creating loop to output diagnostics
-  params_list <- c(paste0("y0[",np,",",iso(x),"]"), paste0("y1[",np,",",iso(x),"]"), paste0("t1[",np,",",iso(x),"]"), paste0("alpha[",np,",",iso(x),"]"), paste0("shape[",np,",",iso(x),"]"))
-  visualize_jags_sub <- visualize_jags%>%
-    mutate(Parameter_char = as.character(Parameter)) %>%
-    filter(Parameter_char %in% params_list)
+############# FUNCTIONS for diagnostics
+### Writing functions to plot histogram, density plot, and traceplot of the jags outputs 
+plot_jags_hist <- function(iso=unique(visualize_jags_sub$Iso_type), param=unique(visualize_jags_sub$Parameter_sub)) {
+  visualize_jags_plot <- visualize_jags_sub %>%
+    filter(Iso_type %in% iso) %>%
+    filter(Parameter_sub %in% param)
   ## Creating historgrams
-  ggs_histogram(visualize_jags_sub)
+  ggs_histogram(visualize_jags_plot)
 }
-plot_jags_dens <- function(x) {
-  #Creating loop to output diagnostics
-  params_list <- c(paste0("y0[",np,",",iso(x),"]"), paste0("y1[",np,",",iso(x),"]"), paste0("t1[",np,",",iso(x),"]"), paste0("alpha[",np,",",iso(x),"]"), paste0("shape[",np,",",iso(x),"]"))
-  visualize_jags_sub <- visualize_jags%>%
-    mutate(Parameter_char = as.character(Parameter)) %>%
-    filter(Parameter_char %in% params_list)
-  ggs_density(visualize_jags_sub)
+#Example call
+# plot_jags_hist("hlya_IgA", "y1")
+
+plot_jags_dens <- function(iso=unique(visualize_jags_sub$Iso_type), param=unique(visualize_jags_sub$Parameter_sub)) {
+  visualize_jags_plot <- visualize_jags_sub %>%
+    filter(Iso_type %in% iso) %>%
+    filter(Parameter_sub %in% param)
+  ## Creating density plots 
+  ggs_density(visualize_jags_plot)
 }
-plot_jags_trace <- function(x) {
+#Example call
+# plot_jags_dens("hlya_IgA", "alpha")
+
+plot_jags_trace <- function(iso=unique(visualize_jags_sub$Iso_type), param=unique(visualize_jags_sub$Parameter_sub)) {
   #Creating loop to output diagnostics
-  params_list <- c(paste0("y0[",np,",",iso(x),"]"), paste0("y1[",np,",",iso(x),"]"), paste0("t1[",np,",",iso(x),"]"), paste0("alpha[",np,",",iso(x),"]"), paste0("shape[",np,",",iso(x),"]"))
-  visualize_jags_sub <- visualize_jags%>%
-    mutate(Parameter_char = as.character(Parameter)) %>%
-    filter(Parameter_char %in% params_list)
-  ## Traceplots
-  ggs_traceplot(visualize_jags_sub)
+  visualize_jags_plot <- visualize_jags_sub %>%
+    filter(Iso_type %in% iso) %>%
+    filter(Parameter_sub %in% param)
+  ## Creating traceplot
+  ggs_traceplot(visualize_jags_plot)
 }
-summ_jags <- function(x) {
+#Example call
+# plot_jags_trace("hlya_IgA", "alpha")
+
+summ_jags <- function(iso=unique(visualize_jags_sub$Iso_type), param=unique(visualize_jags_sub$Parameter_sub)) {
   #Creating loop to output diagnostics
-  params_list <- c(paste0("y0[",np,",",iso(x),"]",), paste0("y1[",np,",",iso(x),"]"), paste0("t1[",np,",",iso(x),"]"), paste0("alpha[",np,",",iso(x),"]"), paste0("shape[",np,",",iso(x),"]"))
-  visualize_jags_sub <- visualize_jags%>%
-    mutate(Parameter_char = as.character(Parameter)) %>%
-    filter(Parameter_char %in% params_list)
+  visualize_jags_plot <- visualize_jags_sub %>%
+    filter(Iso_type %in% iso) %>%
+    filter(Parameter_sub %in% param)
   ### Short summary 
   ci(visualize_jags_sub)[,c(1:6)]
 }
+#Example call
+# summ_jags
+# For these functions you must input the antigen name and the parameter and it should output the specific 
+# diagnostic for that combination. 
 
-
-plot_jags_trace("hlya_IgA")
-plot_jags_dens("hlya_IgA")
-plot_jags_hist("hlya_IgA")
-summ_jags("hlya_IgA")
-
-###### Creating the same functions but with the parameter as the input, rather than antigen type
-Ag_list <- attributes(longdata)$antigens
-plot_jags_hist <- function(x) {
-  #Creating loop to output diagnostics
-  for (i in Ag_list) {
-  params_list <- c(paste0(i,"[",np,",",i,"]"), paste0(i,"[",np,",",iso(x),"]"), paste0(i,"[",np,",",iso(x),"]"), paste0(i,"[",np,",",iso(x),"]"), paste0(i,"[",np,",",iso(x),"]"))
-  }
-  
-  visualize_jags_sub <- visualize_jags%>%
-    mutate(Parameter_char = as.character(Parameter)) %>%
-    filter(Parameter_char %in% params_list)
-  ## Creating historgrams
-  ggs_histogram(visualize_jags_sub)
-}
-plot_jags_dens <- function(x) {
-  #Creating loop to output diagnostics
-  params_list <- c(paste0("y0[",np,",",iso(x),"]"), paste0("y1[",np,",",iso(x),"]"), paste0("t1[",np,",",iso(x),"]"), paste0("alpha[",np,",",iso(x),"]"), paste0("shape[",np,",",iso(x),"]"))
-  visualize_jags_sub <- visualize_jags%>%
-    mutate(Parameter_char = as.character(Parameter)) %>%
-    filter(Parameter_char %in% params_list)
-  ggs_density(visualize_jags_sub)
-}
-plot_jags_trace <- function(x) {
-  #Creating loop to output diagnostics
-  params_list <- c(paste0("y0[",np,",",iso(x),"]"), paste0("y1[",np,",",iso(x),"]"), paste0("t1[",np,",",iso(x),"]"), paste0("alpha[",np,",",iso(x),"]"), paste0("shape[",np,",",iso(x),"]"))
-  visualize_jags_sub <- visualize_jags%>%
-    mutate(Parameter_char = as.character(Parameter)) %>%
-    filter(Parameter_char %in% params_list)
-  ## Traceplots
-  ggs_traceplot(visualize_jags_sub)
-}
-summ_jags <- function(x) {
-  #Creating loop to output diagnostics
-  params_list <- c(paste0("y0[",np,",",iso(x),"]",), paste0("y1[",np,",",iso(x),"]"), paste0("t1[",np,",",iso(x),"]"), paste0("alpha[",np,",",iso(x),"]"), paste0("shape[",np,",",iso(x),"]"))
-  visualize_jags_sub <- visualize_jags%>%
-    mutate(Parameter_char = as.character(Parameter)) %>%
-    filter(Parameter_char %in% params_list)
-  ### Short summary 
-  ci(visualize_jags_sub)[,c(1:6)]
-}
-
-
-######################################### SS Diagonstic code 
-
-
-### Cholera Analysis --------------
-# Creating an output for each of the stratum
-jags.post_young <- jags.post
-jags.post_old <- jags.post
-jags.post_vaccine <- jags.post
-jags.post_case <- jags.post
-
+######## END OF FUNCTIONS
+######################################### SS diagonstic code end
