d21a5890aab512bdc2b50ca686ea3b8d9b97cbb3
sschildhauer <sschildhauer@ucdavis.edu>
2024-08-23 16:40:26 +0700

    Adding cholera csv to ignore. Adding diagnostic functions individually to R folder.

---
+++ R/plot_jags_traceplotdx.R
@@ -0,0 +1,50 @@
+
+#################################### SS diagonstic code begin
+# -- Diagnostics using ggmcmc package
+# -- Compiling into dataframe using ggs()
+plot_jags_trace <- function(data=jags.post,iso=unique(visualize_jags_sub$Iso_type), param=unique(visualize_jags_sub$Parameter_sub)) {
+  #Creating a ggs object, which is part of the jags package to create diagnostic plots
+  visualize_jags <- ggs(data[["mcmc"]])
+  
+  # Creating columns to subset the outputs -- using regular expressions to do so from the output
+  iso_dat <- data.frame(attributes(longdata)$antigens) 
+  iso_dat <- iso_dat %>% mutate(Subnum = as.numeric(row.names(iso_dat)))
+  visualize_jags <- visualize_jags %>%
+    mutate(Subnum = sub('.*,','',Parameter),
+           Parameter_sub = sub('\\[.*','',Parameter),
+           Subject = sub('\\,.*','',Parameter)) %>%
+    mutate(Subnum = as.numeric(sub("\\].*",'',Subnum)),
+           Subject = sub(".*\\[",'',Subject))
+  
+  # Merging iso dat in to ensure we are plotting the name of the antigen correctly
+  visualize_jags <- left_join(visualize_jags, iso_dat, by="Subnum") 
+  # visualize_jags <- merge(visualize_jags, iso_dat, "Subnum", all=T) 
+  visualize_jags <- visualize_jags %>%
+    rename(c("Iso_type"="attributes.longdata..antigens"))%>%
+    select(!c("Subnum"))
+  # Setting subset for the "new person" -- setting it to the final sample
+  np <- as.character(longdata$nsubj)
+  visualize_jags_sub <- visualize_jags %>%
+    filter(Subject == np)
+  #Creating a label
+  visualize_jags <- visualize_jags %>%
+    mutate(Label = paste0(Iso_type,", ",Parameter_sub)) 
+
+  #Creating loop to output diagnostics
+  visualize_jags_plot <- visualize_jags_sub %>%
+    filter(Iso_type %in% iso) %>%
+    filter(Parameter_sub %in% param) %>%
+    # Changing parameter name to reflect the input 
+    mutate(Parameter=paste0("antigen=",Iso_type,", parameter=", Parameter_sub))
+  ## Creating traceplot
+  traceplot <- ggs_traceplot(visualize_jags_plot)
+  traceplot
+}
+
+#Example call
+# plot_jags_trace(jags.post,"hlya_IgA","y1")
+# plot_jags_trace(jags.post,c("hlya_IgG","hlya_IgA"),"y1")
+
+
+
+
