03ad9c35f6d364044118e57e2e95dd55b3d307e6
sschildhauer <sschildhauer@ucdavis.edu>
2025-02-06 17:48:25 -0800

    Wrote diagonstic function for density curves that can be stratified. Updated news, documentation.

--- R/plot_jags_densitydx.R
+++ R/plot_jags_densitydx.R
@@ -1,49 +1,92 @@
 
+#' @title Density Plot Diagnostics
+#' @author Sam Schildhauer
+#' @description
+#'  plot_jags_dens() takes a [list] output from the [serodynamics::run_mod()]
+#'  function to create density plots for each chain run in the mcmc estimation.
+#'  Defaults will produce every combination of antigen/antibody, parameters,
+#'  and stratifications, unless otherwise specified.
+#'  Antigen/antibody combinations and stratifications will vary by analysis.
+#'  The antibody dynamic curve includes the following parameters:
+#'  - y0 = baseline antibody concentration
+#'  - y1 = peak antibody concentration
+#'  - t1 = time to peak
+#'  - r = shape parameter
+#'  - alpha = decay rate
+#' @param name description
+#' @param data A [base::list()] outputted from run_mod().
+#' @param iso Specify [string] to produce plots of only a specific
+#' antigen/antibody combination, entered with quotes. Default outputs all
+#' antigen/antibody combinations.
+#' @param param Specify [string] to produce plots of only a specific parameter,
+#' entered with quotes. Options include:
+#' - `alpha` = posterior estimate of decay rate
+#' - `r` = posterior estimate of shape parameter
+#' - `t1` = posterior estimate of time to peak
+#' - `y0` = posterior estimate of baseline antibody concentration
+#' - `y1` = posterior estimate of peak antibody concentration
+#' @param strat Specify [string] to produce plots of specific stratification
+#' entered in quotes.
+#' @return A [list] of [] objects producing density plots for all the specified
+#' input.
+#' @export
+#' @examples
+#' plot_jags_dens(
+#'     data = [list], #A [serodynamics::run_mod()] [list] output
+#'     iso = "hlya_IgG", #A [string] specifying antigen/antibody of interest.
+#'     Default are all antigen/antibody combinations.
+#'     param = "alpha",  #A [string] specifying parameter of interest. Default
+#'     is all parameters.
+#'     strat = 4),  #A [string] specifying stratification of interest. Default
+#'     is all stratifications.
 
-# Density Plots
-# -- Diagnostics using ggmcmc package
-# -- Compiling into dataframe using ggs()
-plot_jags_dens <- function(data=jags.post,iso=unique(visualize_jags_sub$Iso_type), param=unique(visualize_jags_sub$Parameter_sub)) {
-  #Creating a ggs object, which is part of the jags package to create diagnostic plots
-  visualize_jags <- ggs(data[["mcmc"]])
-  
-  # Creating columns to subset the outputs -- using regular expressions to do so from the output
-  iso_dat <- data.frame(attributes(longdata)$antigens) 
-  iso_dat <- iso_dat %>% mutate(Subnum = as.numeric(row.names(iso_dat)))
-  visualize_jags <- visualize_jags %>%
-    mutate(Subnum = sub('.*,','',Parameter),
-           Parameter_sub = sub('\\[.*','',Parameter),
-           Subject = sub('\\,.*','',Parameter)) %>%
-    mutate(Subnum = as.numeric(sub("\\].*",'',Subnum)),
-           Subject = sub(".*\\[",'',Subject))
-  
-  # Merging iso dat in to ensure we are plotting the name of the antigen correctly
-  visualize_jags <- left_join(visualize_jags, iso_dat, by="Subnum") 
-  # visualize_jags <- merge(visualize_jags, iso_dat, "Subnum", all=T) 
-  visualize_jags <- visualize_jags %>%
-    rename(c("Iso_type"="attributes.longdata..antigens"))%>%
-    select(!c("Subnum"))
-  # Setting subset for the "new person" -- setting it to the final sample
-  np <- as.character(longdata$nsubj)
-  visualize_jags_sub <- visualize_jags %>%
-    filter(Subject == np)
-  #Creating a label
-  visualize_jags <- visualize_jags %>%
-    mutate(Label = paste0(Iso_type,", ",Parameter_sub)) 
-  
-  #Creating loop to output diagnostics
-  visualize_jags_plot <- visualize_jags_sub %>%
-    filter(Iso_type %in% iso) %>%
-    filter(Parameter_sub %in% param) %>%
-    # Changing parameter name to reflect the input 
-    mutate(Parameter=paste0("antigen=",Iso_type,", parameter=", Parameter_sub))
-  ## Creating density plot
-  densplot <- ggs_density(visualize_jags_plot)
-  densplot
+plot_jags_dens <- function(data,
+                           iso = unique(visualize_jags_sub$Iso_type),
+                           param = unique(visualize_jags_sub$Parameter_sub),
+                           strat = NA) {
+  visualize_jags <- data[["curve_params"]]
+  attributes_jags <- data[["attributes"]]
+
+  if (is.na(strat)) {
+    stratification <- c("No stratification")
+  } else {
+    stratification <- unique(visualize_jags$Stratification)
+  }
+
+  dens_strat_list <- list()
+  for (i in stratification) {
+
+    if (i == "No stratification") {
+      visualize_jags_sub <- visualize_jags
+    } else {
+      visualize_jags_sub <- visualize_jags |>
+        filter(.data$Stratification == i)
+    }
+
+    # Creating open list to store ggplots
+    density_out <- list()
+    # Looping through the isos
+    for (j in iso) {
+      visualize_jags_plot <- visualize_jags_sub %>%
+        filter(.data$Iso_type == j)
+
+      # Will not loop through parameters, as we may want each to show on the
+      # same plot by default.
+      visualize_jags_plot <- visualize_jags_plot %>%
+        filter(.data$Parameter_sub %in% param)
+
+      visualize_jags_plot <- visualize_jags_plot |>
+        # Changing parameter name to reflect the input
+        mutate(Parameter = paste0("iso = ", j, ", parameter = ",
+                                  .data$Parameter_sub))
+      # Assigning attributes, which are needed to run ggs_density
+      attributes(visualize_jags_plot) <- c(attributes(visualize_jags_plot),
+                                           attributes_jags)
+      # Creating density plot
+      densplot <- ggmcmc::ggs_density(visualize_jags_plot)
+      density_out[[j]] <- densplot
+    }
+    dens_strat_list[[i]] <- density_out
+  }
+  dens_strat_list
 }
-
-#Example call
-# plot_jags_dens(jags.post,"hlya_IgA","y1")
-# plot_jags_dens(jags.post,c("hlya_IgG","hlya_IgA"),"y1")
-
-
