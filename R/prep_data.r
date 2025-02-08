a1af9d4ce2cd377bf21c5134e4bd151a1c636066
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-07 18:31:08 -0800

    lint

--- R/prep_data.r
+++ R/prep_data.r
@@ -13,85 +13,72 @@
 #' prepped_data <- prep_data(raw_data)
 prep_data <- function(dataframe) {
   # Ensure the data has the required columns
-  if (!("antigen_iso" %in% names(dataframe)) || !("visit_num" %in% names(dataframe))) {
+  columns_missing <-
+    !("antigen_iso" %in% names(dataframe)) ||
+    !("visit_num" %in% names(dataframe))
+
+  if (columns_missing) {
     stop("Dataframe must contain 'antigen_iso' and 'visit_num' columns")
   }
-  
-  
-  
+
   # Extract unique visits and antigens
-  visits <- 
-    dataframe$visit_num |> 
+  visits <-
+    dataframe$visit_num |>
     as.numeric() |>
     unique() |>
     sort()
-  
-  antigens <- 
-    dataframe$antigen_iso |> 
+
+  antigens <-
+    dataframe$antigen_iso |>
     unique()
-  
-  subjects <- 
+
+  subjects <-
     dataframe |>
     get_subject_ids() |>
     unique()
-  
+
   # Initialize arrays to store the formatted data
   max_visits <- length(visits)
   max_antigens <- length(antigens)
   num_subjects <- length(subjects)
-  
+
   # Define arrays with dimensions to accommodate extra dummy subject
-  
+
   dimnames1 <- list(
     subjects = c(subjects, "newperson"),
     visit_number = paste0("V", visits)
   )
-  
+
   dims1 <- sapply(FUN = length, X = dimnames1)
-  
+
   visit_times <- array(
     NA,
     dim = dims1,
     dimnames = dimnames1
   )
-  
+
   dimnames2 <- list(
     subjects = c(subjects, "newperson"),
     visit_number = paste0("V", visits),
     antigens = antigens
   )
-  
+
   antibody_levels <- array(
     NA,
     dim = c(num_subjects + 1, max_visits, max_antigens),
     dimnames = dimnames2
   )
-  
-  nsmpl <- integer(num_subjects + 1) # Array to store the maximum number of samples per participant
-  
-  # Populate the arrays
-  
-  # for (i in seq_len(num_subjects)) {
-  #   subject_data <- dataframe[dataframe$index_id == subjects[i], ]
-  #   subject_visits <- sort(unique(subject_data$visit_num))
-  #   nsmpl[i] <- length(subject_visits)  # Number of non-missing visits for this participant
-  #
-  #   for (j in seq_along(subject_visits)) {
-  #     for (k in seq_len(max_antigens)) {
-  #       subset <- subject_data[subject_data$visit_num == subject_visits[j] & subject_data$antigen_iso == antigens[k], ]
-  #       if (nrow(subset) > 0) {
-  #         visit_times[i, j] <- subset$timeindays
-  #         antibody_levels[i, j, k] <- log(max(0.01, subset$result))  # Log-transform and handle zeroes
-  #       }
-  #     }
-  #   }
-  # }
-  
+
+  # Array to store the maximum number of samples per participant
+  nsmpl <- integer(num_subjects + 1)
+
   for (i in seq_len(num_subjects)) {
     subject_data <- dataframe[dataframe$index_id == subjects[i], ]
     subject_visits <- sort(unique(subject_data$visit_num))
-    nsmpl[i] <- length(subject_visits) # Number of non-missing visits for this participant
-    
+
+    # Number of non-missing visits for this participant
+    nsmpl[i] <- length(subject_visits)
+
     for (j in seq_along(subject_visits)) {
       for (k in seq_len(max_antigens)) {
         index <- subject_data$visit_num == subject_visits[j] &
@@ -104,29 +91,32 @@ prep_data <- function(dataframe) {
                 "Error at subject: {subjects[i]}, ",
                 "visit: {subject_visits[j]},",
                 "antigen: {antigens[k]}",
-                "- Number of items in {.var {subset |> get_timeindays_var()}} is not 1."
-              ))
-            
+                "- # of items in {.var {get_timeindays_var(subset)}} != 1."
+              )
+            )
           }
           visit_times[i, j] <- subset |> get_timeindays()
-          antibody_levels[i, j, k] <- log(max(0.01, 
-                                              subset |> 
-                                                serocalculator:::get_value())) 
+          antibody_levels[i, j, k] <- log(max(
+            0.01,
+            subset |>
+              serocalculator:::get_value()
+          ))
           # Log-transform and handle zeroes
         }
       }
     }
   }
-  
+
   # Add missing observation for Bayesian inference
   visit_times[num_subjects + 1, 1:3] <- c(5, 30, 90)
   # Ensure corresponding antibody levels are set to NA (explicitly missing)
   antibody_levels[num_subjects + 1, 1:3, ] <- NA
-  nsmpl[num_subjects + 1] <- 3 # Since we manually add three time-points for the dummy subject
-  
-  
-  
-  
+  nsmpl[num_subjects + 1] <- 3
+  # Since we manually add three time-points for the dummy subject
+
+
+
+
   # Return results as a list
   to_return <-
     list(
@@ -135,10 +125,6 @@ prep_data <- function(dataframe) {
       "n_antigen_isos" = max_antigens,
       "nsmpl" = nsmpl,
       "nsubj" = num_subjects + 1
-      
-      
-      # "index_id_names" = subjects,
-      # "antigen_names" = antigens
     ) |>
     structure(
       class = c("prepped_jags_data", "list"),
@@ -146,7 +132,7 @@ prep_data <- function(dataframe) {
       n_antigens = max_antigens,
       ids = c(subjects, "newperson")
     )
-  
+
   # Return results as a list
   return(to_return)
 }
--- R/sim_case_data.R
+++ R/sim_case_data.R
@@ -22,14 +22,13 @@ sim_case_data <- function(
     dist_n_obs = tibble::tibble(n_obs = 1:max_n_obs, prob = 1 / max_n_obs),
     followup_interval = 7,
     followup_variance = 1) {
-  
   case_level_data <-
     tibble::tibble(
       id = seq_len(n) |> as.character(),
       n_obs = sim_n_obs(dist_n_obs, n),
       iter = sample(curve_params$iter, size = n, replace = TRUE)
     )
-  
+
   obs_level_data <-
     case_level_data |>
     dplyr::reframe(
@@ -41,13 +40,12 @@ sim_case_data <- function(
         .data$n_obs
       )
     )
-  
+
   biomarker_level_data <-
     obs_level_data |>
     dplyr::reframe(
       .by = c("id", "visit_num", "obs_time", "iter"),
-      antigen_iso =
-        curve_params |> 
+      antigen_iso = curve_params |>
         serocalculator:::get_biomarker_levels()
     ) |>
     dplyr::left_join(
@@ -57,28 +55,33 @@ sim_case_data <- function(
           curve_params |> serocalculator:::get_biomarker_names_var(),
         "iter"
       )
-    ) |> 
-    rowwise() |> 
+    ) |>
+    rowwise() |>
     mutate(
-      value = ab(t = .data$obs_time,
-                 y0 = .data$y0,
-                 y1 = .data$y1,
-                 t1 = .data$t1,
-                 alpha = .data$alpha, 
-                 shape = .data$r)
-    ) |> 
+      value = ab(
+        t = .data$obs_time,
+        y0 = .data$y0,
+        y1 = .data$y1,
+        t1 = .data$t1,
+        alpha = .data$alpha,
+        shape = .data$r
+      )
+    ) |>
     ungroup()
-  
-  to_return = 
-    biomarker_level_data |> 
+
+  to_return <-
+    biomarker_level_data |>
     dplyr::rename(
       index_id = "id",
-      timeindays = "obs_time") |> 
-    structure(class = c("case_data", class(biomarker_level_data)),
-              subject_id = "index_id",
-              biomarker_var = "antigen_iso",
-              timeindays = "timeindays",
-              value_var = "value")
-  
+      timeindays = "obs_time"
+    ) |>
+    structure(
+      class = c("case_data", class(biomarker_level_data)),
+      subject_id = "index_id",
+      biomarker_var = "antigen_iso",
+      timeindays = "timeindays",
+      value_var = "value"
+    )
+
   return(to_return)
 }
