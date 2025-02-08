2f36b0e7e7155c8c381a906200590ac2d51d149c
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-08 00:09:49 -0800

    more

--- tests/testthat/test-as_case_data.R
+++ tests/testthat/test-as_case_data.R
@@ -2,20 +2,21 @@ test_that(
   desc = "results are consistent",
   code = {
     withr::with_seed(
-      seed = 1,
+      1,
       code = {
         test_obj <-
-          set.seed(1)
-        serocalculator::typhoid_curves_nostrat_100 |>
-          sim_case_data(n = 100) |>
-          as_case_data(
-            id_var = "index_id",
-            biomarker_var = "antigen_iso",
-            time_in_days = "timeindays",
-            value_var = "value"
-          )
+          serocalculator::typhoid_curves_nostrat_100 |>
+          sim_case_data(n = 5)
       }
     )
+
+    test_obj <- test_obj |>
+      as_case_data(
+        id_var = "index_id",
+        biomarker_var = "antigen_iso",
+        time_in_days = "timeindays",
+        value_var = "value"
+      )
 
     test_obj |>
       expect_snapshot_value(style = "deparse")
