a07c04b318916661f800f9ccf3b3307c7510ae8c
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-07 18:39:20 -0800

    cleanup

--- R/get_subject_ids.R
+++ R/get_subject_ids.R
@@ -1,21 +1,18 @@
 get_subject_ids <- function(data) {
-  
   subject_id_varname <- data |> get_subject_id_var()
-    
+
   subject_ids <- data[[subject_id_varname]]
-  
+
   return(subject_ids)
-  
 }
 
 get_subject_id_var <- function(data) {
-  
-  subject_id_varname <- 
+  subject_id_varname <-
     data |> attr("subject_id")
-  
+
   if (is.null(subject_id_varname)) {
     subject_id_varname <- "index_id"
   }
-  
+
   return(subject_id_varname)
 }
--- R/get_timeindays.R
+++ R/get_timeindays.R
@@ -1,15 +1,11 @@
 get_timeindays_var <- function(object) {
-  
   var <- object |> attr("timeindays")
   if (is.null(var)) var <- "timeindays"
   return(var)
-  
 }
 
 get_timeindays <- function(object) {
-  
   var <- object |> get_timeindays_var()
-  
+
   return(object[[var]])
-  
 }
