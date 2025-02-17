968d2c0eb0219194ab80e23c2ceb95b9dffa5f47
sschildhauer <sschildhauer@ucdavis.edu>
2025-02-17 20:45:42 +0700

    Adding traceplot and density plot functions to branch

---
+++ R/plot_jags_tracedx.R
@@ -0,0 +1,110 @@
+
+#' @title Trace Plot Diagnostics
+#' @author Sam Schildhauer
+#' @description
+#'  plot_jags_trace() takes a [list] output from [serodynamics::run_mod()]
+#'  to create trace plots for each chain run in the mcmc estimation.
+#'  Defaults will produce every combination of antigen/antibody, parameters,
+#'  and stratifications, unless otherwise specified.
+#'  Antigen/antibody combinations and stratifications will vary by analysis.
+#'  The antibody dynamic curve includes the following parameters:
+#'  - y0 = baseline antibody concentration
+#'  - y1 = peak antibody concentration
+#'  - t1 = time to peak
+#'  - r = shape parameter
+#'  - alpha = decay rate
+#' @param name description
+#' @param data A [base::list()] outputted from run_mod().
+#' @param iso Specify [string] to produce plots of only a specific
+#' antigen/antibody combination, entered with quotes. Default outputs all
+#' antigen/antibody combinations.
+#' @param param Specify [string] to produce plots of only a specific parameter,
+#' entered with quotes. Options include:
+#' - `alpha` = posterior estimate of decay rate
+#' - `r` = posterior estimate of shape parameter
+#' - `t1` = posterior estimate of time to peak
+#' - `y0` = posterior estimate of baseline antibody concentration
+#' - `y1` = posterior estimate of peak antibody concentration
+#' @param strat Specify [string] to produce plots of specific stratification
+#' entered in quotes.
+#' @return A [list] of [ggplot] objects producing trace plots for all the specified
+#' input.
+#' @export
+#' @examples
+#' if (!is.element(runjags::findjags(), c("", NULL))) {
+#'   library(runjags)
+#'   library(ggmcmc)
+#'   set.seed(1)
+#'   library(dplyr)
+#'   strat1 <- serocalculator::typhoid_curves_nostrat_100 |>
+#'     sim_case_data(n = 100) |>
+#'     mutate(strat = "stratum 2")
+#'   strat2 <- serocalculator::typhoid_curves_nostrat_100 |>
+#'     sim_case_data(n = 100) |>
+#'     mutate(strat = "stratum 1")
+#'
+#'   Dataset <- bind_rows(strat1, strat2)
+#'
+#'   jags_out <- run_mod(
+#'     data = Dataset, # The data set input
+#'     file_mod = fs::path_package("serodynamics", "extdata/model.jags.r"),
+#'     nchain = 4, # Number of mcmc chains to run
+#'     nadapt = 100, # Number of adaptations to run
+#'     nburn = 100, # Number of unrecorded samples before sampling begins
+#'     nmc = 1000,
+#'     niter = 2000, # Number of iterations
+#'     strat = "strat"
+#'   ) # Variable to be stratified
+#' plot_jags_trace(
+#'     data = jags_out, #A [serodynamics::run_mod()] [list] output.
+#'     iso = "hlya_IgG", #A [string] specifying antigen/antibody of interest.
+#'     param = "alpha",  #A [string] specifying parameter of interest.
+#'     strat = "strat")  #A [string] specifying stratification of interest.
+#'     }
+
+plot_jags_trace <- function(data,
+                           iso = unique(visualize_jags_sub$Iso_type),
+                           param = unique(visualize_jags_sub$Parameter_sub),
+                           strat = unique(visualize_jags$Stratification)) {
+  visualize_jags <- data[["curve_params"]]
+  attributes_jags <- data[["attributes"]]
+
+  trace_strat_list <- list()
+  for (i in strat) {
+
+      visualize_jags_sub <- visualize_jags |>
+        dplyr::filter(.data$Stratification == i)
+
+    # Creating open list to store ggplots
+    trace_out <- list()
+    # Looping through the isos
+    for (j in iso) {
+      visualize_jags_plot <- visualize_jags_sub |>
+        dplyr::filter(.data$Iso_type == j)
+
+      # Will not loop through parameters, as we may want each to show on the
+      # same plot by default.
+      visualize_jags_plot <- visualize_jags_plot |>
+        dplyr::filter(.data$Parameter_sub %in% param)
+
+      visualize_jags_plot <- visualize_jags_plot |>
+        # Changing parameter name to reflect the input
+        dplyr::mutate(Parameter = paste0("iso = ", j, ", parameter = ",
+                                         .data$Parameter_sub, ", strat = ",
+                                         i)) |>
+        dplyr::mutate(value = ifelse(Parameter_sub == "y1" | Parameter_sub == "alpha", 
+                             log(value), value))
+      # Assigning attributes, which are needed to run ggs_trace
+      attributes(visualize_jags_plot) <- c(attributes(visualize_jags_plot),
+                                           attributes_jags)
+      # Creating trace plot
+      traceplot <- ggmcmc::ggs_traceplot(visualize_jags_plot) +
+        theme_bw()
+      trace_out[[j]] <- traceplot
+    }
+    trace_strat_list[[i]] <- trace_out
+  }
+  trace_strat_list
+}
+
+plot_jags_trace(Stratified, iso="ctb_IgA",)
