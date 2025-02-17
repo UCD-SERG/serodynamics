d1b7410dc809312b62bd595ea71d9b6f9b64a300
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-17 13:33:29 -0800

    new data load script

---
+++ data-raw/dataprep.serokinetics.r
@@ -0,0 +1,20 @@
+
+#creating a mini data set here for the new serodynamcis package 
+#02-13-25
+#nepal, prospective cases only, hlye igg and iga only
+setwd("~/Library/CloudStorage/OneDrive-UniversityofCalifornia,Davis/Research/EF_longitudinal/Data for serokinetics")
+library(tidyverse)
+
+dcase <- read_csv("/Users/kristenaiemjoy/Documents/GitHub/serodynamics/inst/extdata/elisa_clean_2024-06-05.csv") %>%
+  filter(Arm == "Prospective Cases" & Country == "Nepal") %>%
+  mutate(antigen_iso = paste(elisa_antigen, "_", elisa_antbdy_iso, sep=""),
+         antigen_iso = factor(antigen_iso)) %>% 
+  filter(antigen_iso == "HlyE_IgG" | antigen_iso == "HlyE_IgA") %>%
+  mutate(person_id = paste0("sees_npl_", as.numeric(as.factor(pid))),
+         sample_id = paste0("N000_", as.numeric(as.factor(sid)))) %>%
+  select(Country, person_id, sample_id, bldculres, antigen_iso, studyvisit,  dayssincefeveronset,  result) 
+
+  
+  
+
+write_csv(dcase, "SEES_Case_Nepal_ForSeroKinetics_02-13-2025.csv")
