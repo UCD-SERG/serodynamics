9dc23473c989dc60a24c781842ac1a4419910d27
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-17 20:09:17 -0800

    shorter snapshots

--- tests/testthat/test-run_mod.R
+++ tests/testthat/test-run_mod.R
@@ -4,35 +4,78 @@ test_that(
     library(runjags)
     dataset <- serodynamics_example(
       "SEES_Case_Nepal_ForSeroKinetics_02-13-2025.csv"
-    ) |> 
-      readr::read_csv() |> 
+    ) |>
+      readr::read_csv() |>
       dplyr::mutate(
         .by = person_id,
-        visit_num = dplyr::row_number()) |> 
+        visit_num = dplyr::row_number()
+      ) |>
       as_case_data(
         id_var = "person_id",
         biomarker_var = "antigen_iso",
         value_var = "result",
         time_in_days = "dayssincefeveronset"
       )
-    
+
     results <- run_mod(
       data = dataset, # The data set input
       file_mod = serodynamics_example("model.jags"),
       nchain = 2, # Number of mcmc chains to run
       nadapt = 100, # Number of adaptations to run
       nburn = 100, # Number of unrecorded samples before sampling begins
-      nmc = 100,
-      niter = 100, # Number of iterations
+      nmc = 10,
+      niter = 10, # Number of iterations
       strat = "bldculres" # Variable to be stratified
     ) |>
       suppressWarnings() |>
-      magrittr::use_series("curve_params")
-    
-    results |> 
+      magrittr::use_series("curve_params") |>
+      filter(.data$Iteration %in% 1:10)
+
+    results |>
       expect_snapshot_value(style = "serialize")
-    
+
     results |>
       ssdtools:::expect_snapshot_data("strat-curve-params")
+  }
+)
+
+test_that(
+  desc = "results are consistent without stratification",
+  code = {
+    library(runjags)
+    dataset <- serodynamics_example(
+      "SEES_Case_Nepal_ForSeroKinetics_02-13-2025.csv"
+    ) |>
+      readr::read_csv() |>
+      dplyr::mutate(
+        .by = person_id,
+        visit_num = dplyr::row_number()
+      ) |>
+      as_case_data(
+        id_var = "person_id",
+        biomarker_var = "antigen_iso",
+        value_var = "result",
+        time_in_days = "dayssincefeveronset"
+      )
+
+    results <- run_mod(
+      data = dataset, # The data set input
+      file_mod = serodynamics_example("model.jags"),
+      nchain = 2, # Number of mcmc chains to run
+      nadapt = 100, # Number of adaptations to run
+      nburn = 100, # Number of unrecorded samples before sampling begins
+      nmc = 10,
+      niter = 10, # Number of iterations
+      strat = NA # Variable to be stratified
+    ) |>
+      suppressWarnings() |>
+      magrittr::use_series("curve_params") |>
+      filter(.data$Iteration %in% 1:10)
+
+    results |>
+      expect_snapshot_value(style = "serialize")
+
+    results |>
+      ssdtools:::expect_snapshot_data("nostrat-curve-params")
   }
 )
