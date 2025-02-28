a026232ac1430dae8956d0bd65279b7fd18c9e2b
sschildhauer <sschildhauer@ucdavis.edu>
2025-02-28 17:08:17 +0700

    Adding nepal_sees_jags_post preparation documentation to data_raw folder

---
+++ data-raw/nepal_sees_jags_post.R
@@ -0,0 +1,24 @@
+nepal_sees <- readr::read_csv(
+                              here::here() |>
+                                fs::path("/inst/extdata/
+                                         SEES_Case_Nepal_ForSeroKinetics_
+                                         02-13-2025.csv"))
+
+dataset <- nepal_sees |>
+  as_case_data(id_var = "person_id",
+               biomarker_var = "antigen_iso",
+               value_var = "result",
+               time_in_days = "dayssincefeveronset")
+
+nepal_sees_jags_post <- run_mod(
+  data = dataset, # The data set input
+  file_mod = fs::path_package("serodynamics", "extdata/model.jags"),
+  nchain = 2, # Number of mcmc chains to run
+  nadapt = 100, # Number of adaptations to run
+  nburn = 100, # Number of unrecorded samples before sampling begins
+  nmc = 500,
+  niter = 1000, # Number of iterations
+  strat = "bldculres" # Variable to be stratified
+)
+
+usethis::use_data(nepal_sees_jags_post, overwrite = TRUE)
