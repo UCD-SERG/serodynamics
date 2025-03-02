5b9c1a12861ee40880da609b191d21c2068dcf9d
sschildhauer <sschildhauer@ucdavis.edu>
2025-03-02 22:08:04 +0700

    Updating test that using stored nepal_sees_jags_post object

--- tests/testthat/test-plot_jags_densitydx.R
+++ tests/testthat/test-plot_jags_densitydx.R
@@ -3,32 +3,16 @@ test_that(
   desc = "results are consistent with ggplot output",
   code = {
     library(runjags)
-    set.seed(1)
     library(dplyr)
-    strat1 <- serocalculator::typhoid_curves_nostrat_100 |>
-      sim_case_data(n = 100) |>
-      mutate(strat = "stratum 2")
-    strat2 <- serocalculator::typhoid_curves_nostrat_100 |>
-      sim_case_data(n = 100) |>
-      mutate(strat = "stratum 1")
+    library(here)
+    set.seed(1)
 
-    dataset <- bind_rows(strat1, strat2)
+    data <- serodynamics::nepal_sees_jags_post
 
     withr::with_seed(
       1,
       code = {
-        test_typhoid_data <- run_mod(
-          data = dataset, #The data set input
-          file_mod = fs::path_package("serodynamics", "extdata/model.jags"),
-          nchain = 2, #Number of mcmc chains to run
-          nadapt = 100, #Number of adaptations to run
-          nburn = 100, #Number of unrecorded samples before sampling begins
-          nmc = 100,
-          niter = 100, #Number of iterations
-          strat = "strat"
-        )  |>
-          suppressWarnings()
-        results <- plot_jags_dens(results)
+        results <- plot_jags_dens(data)
       }
     ) |>
       # Testing for any errors
@@ -36,6 +20,6 @@ test_that(
     # Test to ensure output is a list object
     expect_true(is.list(results))
     # Test to ensure that a piece of the list is a ggplot object
-    vdiffr::expect_doppelganger(results$`stratum 1`$HlyE_IgA)
+    vdiffr::expect_doppelganger("typhoid_plot", results$typhi$HlyE_IgA)
   }
 )
