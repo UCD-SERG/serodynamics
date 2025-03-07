e522ddd288db626a333c974330852f3a5da1c4fa
sschildhauer <sschildhauer@ucdavis.edu>
2025-03-07 13:48:34 +0700

    Fixing example again

--- inst/examples/examples-plot_jags_effectivedx.R
+++ inst/examples/examples-plot_jags_effectivedx.R
@@ -3,8 +3,7 @@ if (!is.element(runjags::findjags(), c("", NULL))) {
 
   data <- serodynamics::nepal_sees_jags_post
 
-  plot_jags_effect(
-                   data = data,
+  plot_jags_effect(data = data,
                    iso = "HlyE_IgA",
                    param = "alpha",
                    strat = "typhi")
