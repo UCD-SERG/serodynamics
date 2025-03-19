b8d8f640f12d59a2a9be5897fa6c414f89adbb87
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-03-19 00:13:09 -0700

    more notes

--- tests/testthat/test-runjags.R
+++ tests/testthat/test-runjags.R
@@ -56,5 +56,7 @@ test_that("results are consistent with our model", {
   
   # running this on posit cloud (linux), the first discrepancy 
   # occurs at iteration 19, when person 6's model changes.
+  # windows and mac agree when adapt = 0, but when adapt = 1000,
+  # all three operating systems differ
   
 })
