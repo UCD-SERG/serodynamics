f3c40cd1ba9372e9479c956a2d418a34a9c1b8b3
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-03-18 22:36:28 -0700

    fixture

---
+++ tests/testthat/fixtures/make-example-runjags-inputs.R
@@ -0,0 +1,12 @@
+set.seed(1)
+strat1 <- serocalculator::typhoid_curves_nostrat_100 |>
+  sim_case_data(n = 20, antigen_isos = "HlyE_IgA") |>
+  mutate(strat = "stratum 2")
+longdata <- prep_data(strat1, add_newperson = FALSE)
+priors <- prep_priors(max_antigens = longdata$n_antigen_isos)
+
+tomonitor <- c("y0", "y1", "t1", "alpha", "shape")
+longdata |> 
+  readr::write_rds(
+    testthat::test_path("fixtures", "example_runjags_inputs.rds")
+  )
