c7148953cec301153dbad296a0c32563c61a4d48
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-03-18 22:45:15 -0700

    more

--- tests/testthat/fixtures/make-example-runjags-inputs.R
+++ tests/testthat/fixtures/make-example-runjags-inputs.R
@@ -6,7 +6,16 @@ longdata <- prep_data(strat1, add_newperson = FALSE)
 priors <- prep_priors(max_antigens = longdata$n_antigen_isos)
 
 tomonitor <- c("y0", "y1", "t1", "alpha", "shape")
+
+old <- 
+  readr::read_rds(
+    testthat::test_path("fixtures", "example_runjags_inputs.rds")
+  )
+
+print(all.equal(longdata, old))
+
 longdata |> 
   readr::write_rds(
     testthat::test_path("fixtures", "example_runjags_inputs.rds")
   )
+
