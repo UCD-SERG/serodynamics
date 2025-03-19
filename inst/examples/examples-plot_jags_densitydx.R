066cebb6ffc9f1746f26b32365ca24ec026f7282
sschildhauer <sschildhauer@ucdavis.edu>
2025-03-19 13:26:22 -0700

    Updating example code

--- inst/examples/examples-plot_jags_densitydx.R
+++ inst/examples/examples-plot_jags_densitydx.R
@@ -1,11 +1,7 @@
 
-if (!is.element(runjags::findjags(), c("", NULL))) {
-
   data <- serodynamics::nepal_sees_jags_post
 
   plot_jags_dens(
                  data = data,
                  iso = "HlyE_IgA",
-                 param = "alpha",
                  strat = "typhi")
-}
