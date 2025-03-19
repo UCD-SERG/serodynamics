b281e7dc51f34addf98e36b25093858fa9d9fbeb
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-03-18 23:03:36 -0700

    more

--- tests/testthat/test-runjags.R
+++ tests/testthat/test-runjags.R
@@ -2,6 +2,7 @@ test_that("results are consistent", {
   
   # set.seed(1)
   results <- runjags:::example_runjags()
+  results |> plot(vars = "c", plot.type = "trace")
   results[["mcmc"]] |> 
     ggmcmc::ggs() |> 
     dplyr::filter(Iteration %in% 1:2) |> 
@@ -13,28 +14,28 @@ test_that("results are consistent with our model", {
   
   set.seed(1)
   strat1 <- serocalculator::typhoid_curves_nostrat_100 |>
-    sim_case_data(n = 20, antigen_isos = "HlyE_IgA") |>
+    sim_case_data(n = 9, antigen_isos = "HlyE_IgA") |>
     mutate(strat = "stratum 2")
   longdata <- prep_data(strat1, add_newperson = FALSE)
   priors <- prep_priors(max_antigens = longdata$n_antigen_isos)
   
-  tomonitor <- c("y0", "y1", "t1", "alpha", "shape")
-  
+  tomonitor <- c("y0", "y1", "t1", "alpha", "r")
+  set.seed(1)
   jags_post <- runjags::run.jags(
     model = serodynamics_example("model.jags"),
     data = c(longdata, priors),
     inits = initsfunction,
-    method = "simple",
-    adapt = 50,
+    method = "rjags",
+    adapt = 1000,
     burnin = 1,
     thin = 1,
-    sample = 2,
+    sample = 100,
     n.chains = 1,
     monitor = tomonitor,
-    summarise = FALSE
+    summarise = TRUE
   ) |> 
     suppressWarnings()
-  
+  plot(jags_post, vars = "y1[1,1]")
   samples <- jags_post[["mcmc"]] |> 
     ggmcmc::ggs() |> 
     dplyr::filter(Iteration %in% 1:2) |> 
