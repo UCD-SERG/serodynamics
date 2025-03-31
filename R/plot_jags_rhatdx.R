42549ed0945d3a236168e4816ea572f3a13943a6
sschildhauer <sschildhauer@ucdavis.edu>
2025-03-31 12:50:52 -0700

    Adding plot_jags_rhat that was deleted

---
+++ R/plot_jags_rhatdx.R
@@ -0,0 +1,85 @@
+
+#' @title Rhat Plot Diagnostics
+#' @author Sam Schildhauer
+#' @description
+#'  plot_jags_Rhat() takes a [list] output from [serodynamics::run_mod()]
+#'  to produce dotplots of potential scale reduction factors (Rhat) for each
+#'  chain run in the mcmc estimation. Rhat values analyze the spread of chains
+#'  compared to pooled values with a goal of observing rhat < 1.10 for
+#'  convergence. 
+#'  Defaults will produce every combination of antigen/antibody, parameters,
+#'  and stratifications, unless otherwise specified.
+#'  Antigen/antibody combinations and stratifications will vary by analysis.
+#'  The antibody dynamic curve includes the following parameters:
+#'  - y0 = baseline antibody concentration
+#'  - y1 = peak antibody concentration
+#'  - t1 = time to peak
+#'  - r = shape parameter
+#'  - alpha = decay rate
+#' @param data A [list] outputted from run_mod().
+#' @param iso Specify [character] string to produce plots of only a
+#' specific antigen/antibody combination, entered with quotes. Default outputs
+#' all antigen/antibody combinations.
+#' @param param Specify [character] string to produce plots of only a
+#' specific parameter, entered with quotes. Options include:
+#' - `alpha` = posterior estimate of decay rate
+#' - `r` = posterior estimate of shape parameter
+#' - `t1` = posterior estimate of time to peak
+#' - `y0` = posterior estimate of baseline antibody concentration
+#' - `y1` = posterior estimate of peak antibody concentration
+#' @param strat Specify [character] string to produce plots of specific
+#' stratification entered in quotes.
+#' @return A [list] of [ggplot2::ggplot] objects producing dotplots with rhat
+#' values for all the specified input.
+#' @export
+#' @example inst/examples/examples-plot_jags_rhatdx.R
+
+plot_jags_Rhat <- function(data,  # nolint: object_name_linter
+                           iso = unique(data$curve_params$Iso_type),
+                           param = unique(data$curve_params$Parameter_sub),
+                           strat = unique(data$curve_params$Stratification)) {
+  visualize_jags <- data[["curve_params"]]
+  attributes_jags <- data[["attributes"]]
+  
+  rhat_strat_list <- list()
+  for (i in strat) {
+    
+    visualize_jags_sub <- visualize_jags |>
+      dplyr::filter(.data$Stratification == i)
+    
+    # Creating open list to store ggplots
+    rhat_out <- list()
+    # Looping through the isos
+    for (j in iso) {
+      visualize_jags_plot <- visualize_jags_sub |>
+        dplyr::filter(.data$Iso_type == j)
+      
+      # Will not loop through parameters, as we may want each to show on the
+      # same plot by default.
+      visualize_jags_plot <- visualize_jags_plot |>
+        dplyr::filter(.data$Parameter_sub %in% param)
+      
+      visualize_jags_plot <- visualize_jags_plot |>
+        # Changing parameter name to reflect the input
+        dplyr::mutate(Parameter = .data$Parameter_sub,
+                      value = log(.data$value))
+      # Assigning attributes, which are needed to run ggs_rhat
+      attributes(visualize_jags_plot) <- c(attributes(visualize_jags_plot),
+                                           attributes_jags)
+      # Creating rhat dotplots
+      rhatplot <- ggmcmc::ggs_Rhat(visualize_jags_plot) +
+        ggplot2::theme_bw() +
+        ggplot2::labs(title = ifelse(j == "None", 
+                                     paste0("Rhat value: ag/iso = ", j),
+                                     paste0("Rhat value: ag/iso = ", 
+                                            j, "; strata =  ", i)))
+      rhat_out[[j]] <- rhatplot
+    }
+    rhat_strat_list[[i]] <- rhat_out
+  }
+  #Printing only one plot if only one exists.
+  if (sum(lengths(rhat_strat_list)) == 1) {
+    rhat_strat_list <- rhat_strat_list[[1]][[iso]]
+  } 
+  rhat_strat_list
+}
