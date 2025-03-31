61f73eaa1225c87c7ef66f5040f5c162dfe26303
sschildhauer <sschildhauer@ucdavis.edu>
2025-03-31 13:19:55 -0700

    Updating to include priors as argument in run_mod

--- R/Run_Mod.R
+++ R/Run_Mod.R
@@ -19,9 +19,21 @@
 #' @param nburn An [integer] specifying the number of burn ins before sampling.
 #' @param nmc An [integer] specifying number of samples in posterior chains
 #' @param niter An [integer] specifying number of iterations.
-#' @param strat
-#' A [character] string specifying the stratification variable,
+#' @param strat #' A [character] string specifying the stratification variable, 
 #' entered in quotes.
+#' @param priors A [list] with optional elements:
+#' - "mu_hyp_param": hyperpriors for estimate parameters. If specified must
+#'    be 5 values long.
+#'    - defaults: y0 = 1.0, y1 = 7.0, t1 = 1.0, r = -4.0, alpha = -1.0
+#' - "prec_hyp_param": Precision corresponding to mu_hyp_param. If specified
+#' must 5 values long.
+#'    - defaults: y0 = 1.0, y1 = 0.00001, t1 = 1.0, r = 0.001, alpha = 1.0
+#' - "omega_param" : ?? 
+#'    - defaults: y0 = 1.0, y1 = 50.0, t1 = 1.0, r = 10.0, alpha = 1.0
+#' - "wishdf_param": Wishart distribution degrees of freedom (default = 20.0)
+#' - "prec_logy_hyp_param": array of hyper-parameters for the precision.
+#'    (inverse variance) of the biomarkers, on log-scale
+#'    - defaults = 4.0, 1.0
 #' @return
 #' - A jags.post [list()] object or multiple jags.post [list()]
 #' if stratified. Returned as a [list()] of class [runjags::runjags-class]
