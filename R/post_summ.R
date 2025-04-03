673e460b52a1f958ff92f99b1f2a4e648066e257
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-02 18:30:51 -0700

    Fixing lint and example from post_summ

--- R/post_summ.R
+++ R/post_summ.R
@@ -31,7 +31,7 @@
 #' median, and quantiles (2.5%, 25.0%, 50.0%, 75.0%, 97.5%).
 #' @export
 #' @examples
-#' post_summ(data = serodynamics::nepal_sees_jags_ouput)
+#' post_summ(data = serodynamics::nepal_sees_jags_output)
 
 post_summ <- function(data,
                       iso = unique(data$curve_params$Iso_type),
