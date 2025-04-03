0395543a107c354ff85f9daad7be447213ed7608
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-02 18:17:16 -0700

    Comitting dens example

---
+++ inst/examples/examples-plot_jags_densitydx.R
@@ -0,0 +1,8 @@
+
+data <- serodynamics::nepal_sees_jags_output
+
+# Specifying isotype, parameter, and stratification for traceplot.
+plot_jags_dens(
+                data = data,
+                iso = "HlyE_IgA",
+                strat = "typhi")
