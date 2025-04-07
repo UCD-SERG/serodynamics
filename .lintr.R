ee324decbe4c101970eb04c9d619dceaa2bec32e
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-04-07 15:09:43 -0700

    update lintr

--- .lintr.R
+++ .lintr.R
@@ -1,4 +1,10 @@
-extra_undesirable_functions <- list(
+
+
+undesirable_functions <- 
+  lintr::default_undesirable_functions |> 
+  lintr::modify_defaults(
+    
+  # following https://github.com/r-lib/devtools/blob/2aa51ef/.lintr.R:
   # Base messaging
   "message" = "use cli::cli_inform()",
   "warning" = "use cli::cli_warn()",
@@ -11,7 +17,19 @@ extra_undesirable_functions <- list(
   "cli_alert_danger" = "use cli::cli_inform()",
   "cli_alert_info" = "use cli::cli_inform()",
   "cli_alert_success" = "use cli::cli_inform()",
-  "cli_alert_warning" = "use cli::cli_inform()"
+  "cli_alert_warning" = "use cli::cli_inform()",
+  
+  library = 
+    paste(
+      "\nuse `::`, `usethis::use_import_from()`, or `withr::local_package()`",
+      "instead of modifying the global search path.",
+      "\nSee:\n",
+      "<https://r-pkgs.org/code.html#sec-code-r-landscape> and\n",
+      "<https://r-pkgs.org/testing-design.html#sec-testing-design-self-contained>",
+      "\nfor more details"),
+  
+  structure = NULL
+  
 )
 
 linters <- linters_with_defaults(
@@ -19,12 +37,13 @@ linters <- linters_with_defaults(
   trailing_whitespace_linter = NULL,
   pipe_consistency_linter(pipe = "|>"),
   undesirable_function_linter(
-    fun = c(default_undesirable_functions, extra_undesirable_functions),
+    fun = undesirable_functions,
     symbol_is_undesirable = TRUE
   )
 )
 
-rm(extra_undesirable_functions) # prevents a warning from lintr::read_settings
+# prevent warnings from lintr::read_settings:
+rm(undesirable_functions)
 
 exclusions <- list(
   `data-raw` = list(
