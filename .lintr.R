cc8f063438bb5f9539c1d78215b18b1bbe7b858a
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-04-07 15:13:12 -0700

    lint

--- .lintr.R
+++ .lintr.R
@@ -4,39 +4,39 @@ undesirable_functions <-
   lintr::default_undesirable_functions |> 
   lintr::modify_defaults(
     
-  # following https://github.com/r-lib/devtools/blob/2aa51ef/.lintr.R:
-  # Base messaging
-  "message" = "use cli::cli_inform()",
-  "warning" = "use cli::cli_warn()",
-  "stop" = "use cli::cli_abort()",
-  # rlang messaging
-  "inform" = "use cli::cli_inform()",
-  "warn" = "use cli::cli_warn()",
-  "abort" = "use cli::cli_abort()",
-  # older cli
-  "cli_alert_danger" = "use cli::cli_inform()",
-  "cli_alert_info" = "use cli::cli_inform()",
-  "cli_alert_success" = "use cli::cli_inform()",
-  "cli_alert_warning" = "use cli::cli_inform()",
-  
-  library = 
-    paste(
+    # following https://github.com/r-lib/devtools/blob/2aa51ef/.lintr.R:
+    # Base messaging
+    "message" = "use cli::cli_inform()",
+    "warning" = "use cli::cli_warn()",
+    "stop" = "use cli::cli_abort()",
+    # rlang messaging
+    "inform" = "use cli::cli_inform()",
+    "warn" = "use cli::cli_warn()",
+    "abort" = "use cli::cli_abort()",
+    # older cli
+    "cli_alert_danger" = "use cli::cli_inform()",
+    "cli_alert_info" = "use cli::cli_inform()",
+    "cli_alert_success" = "use cli::cli_inform()",
+    "cli_alert_warning" = "use cli::cli_inform()",
+    
+    library = paste(
       "\nuse `::`, `usethis::use_import_from()`, or `withr::local_package()`",
       "instead of modifying the global search path.",
       "\nSee:\n",
       "<https://r-pkgs.org/code.html#sec-code-r-landscape> and\n",
       "<https://r-pkgs.org/testing-design.html#sec-testing-design-self-contained>",
-      "\nfor more details"),
-  
-  structure = NULL
-  
-)
+      "\nfor more details"
+    ),
+    
+    structure = NULL
+    
+  )
 
-linters <- linters_with_defaults(
+linters <- lintr::linters_with_defaults(
   return_linter = NULL,
   trailing_whitespace_linter = NULL,
-  pipe_consistency_linter(pipe = "|>"),
-  undesirable_function_linter(
+  lintr::pipe_consistency_linter(pipe = "|>"),
+  lintr::undesirable_function_linter(
     fun = undesirable_functions,
     symbol_is_undesirable = TRUE
   )
