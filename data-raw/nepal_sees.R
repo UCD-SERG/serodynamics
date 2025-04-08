c5672daeb8a56561f4c120ab2296f17644d0fe7e
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-08 15:16:48 -0700

    Taking nepal_sees_post out of pkgdown

--- data-raw/nepal_sees.R
+++ data-raw/nepal_sees.R
@@ -9,9 +9,3 @@ nepal_sees <- readr::read_csv(
                time_in_days = "dayssincefeveronset")
 
 usethis::use_data(nepal_sees, overwrite = TRUE)
-
-datacheck <- check |>
-  as_case_data(id_var = "person_id",
-               biomarker_var = "antigen_iso",
-               value_var = "result",
-               time_in_days = "dayssincefeveronset")
