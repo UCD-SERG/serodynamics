21b554973518a9030d0c08b1fd87817c8e3c89a7
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-14 14:13:26 -0700

    Updating documentation for nepal_sees_jags_output

--- R/nepal_sees_jags_output.R
+++ R/nepal_sees_jags_output.R
@@ -12,7 +12,8 @@
 #' A [list] consisting of the following named elements:
 #' \describe{
 #'  \item{curve_params}{A [data.frame] titled `curve_params` that contains the
-#'   posterior distribution}
+#'   posterior distribution for the predictive distribution (newperson) and 
+#'   two modeled subject (sees_npl_1, sees_npl_2)}
 #'  \item{attributes}{A [list] of `attributes` that summarize the jags inputs}
 #' }
 #' @source reference study: <https://doi.org/10.1016/S2666-5247(22)00114-8>
