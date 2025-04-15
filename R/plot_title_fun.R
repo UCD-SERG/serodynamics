85025bc38b10263a415dd815883fe190ba8d9025
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-14 20:50:47 -0700

    Creating function for plotting title in dx functions

---
+++ R/plot_title_fun.R
@@ -0,0 +1,8 @@
+#' Plotting title for diagnostic functions
+
+plot_title_fun <- function(i, j) {
+  subtitle =  ifelse(j == "None", 
+                     paste0("ag/iso = ", j),
+                     paste0("ag/iso = ", 
+                            j, "; strata =  ", i))
+}
