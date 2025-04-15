cda1051e95f9a44c9035103e2be541b45ebd25e2
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-14 20:59:16 -0700

    Updating density plot comment

--- inst/examples/examples-plot_jags_densitydx.R
+++ inst/examples/examples-plot_jags_densitydx.R
@@ -1,7 +1,7 @@
 
 data <- serodynamics::nepal_sees_jags_output
 
-# Specifying isotype, parameter, and stratification for traceplot.
+# Specifying isotype and stratification for traceplot.
 plot_jags_dens(
                data = data,
                iso = "HlyE_IgA",
