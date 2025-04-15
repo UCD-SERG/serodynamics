3dab904d5da86cabbeee0e2c59f33dee4105419d
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-14 22:31:19 -0700

    Updating title_fun doc

--- R/plot_title_fun.R
+++ R/plot_title_fun.R
@@ -1,7 +1,7 @@
 #' Plotting title for diagnostic functions
 #' 
 #' @param i input strata 
-#' @param j input ag/iso combination
+#' @param j input antigen/iso combination
 
 plot_title_fun <- function(i, j) {
   subtitle <-  ifelse(j == "None", 
