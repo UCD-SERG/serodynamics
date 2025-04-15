90c3ff0cf2f59d45a676559ff8d825dab7517d1a
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-14 20:51:01 -0700

    Updating plot_title_fun

--- R/plot_title_fun.R
+++ R/plot_title_fun.R
@@ -1,8 +1,9 @@
 #' Plotting title for diagnostic functions
 
 plot_title_fun <- function(i, j) {
-  subtitle =  ifelse(j == "None", 
+  subtitle <-  ifelse(j == "None", 
                      paste0("ag/iso = ", j),
                      paste0("ag/iso = ", 
                             j, "; strata =  ", i))
+  return(subtitle)
 }
