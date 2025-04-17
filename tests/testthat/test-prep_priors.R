4390493c50cc9f9a3c525d72299f12ec0cd5ba9c
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-17 15:32:54 -0700

    Updating prep_prior test

--- tests/testthat/test-prep_priors.R
+++ tests/testthat/test-prep_priors.R
@@ -5,12 +5,11 @@ test_that("results are consistent", {
 
 
 test_that("priors are modifiable", {
-  priors <- list(mu_hyp_param = c(1.0,  5.0, 0.0, -2.0, -3.0),
-                 prec_hyp_param = c(0.01, 0.01, 0.01, 0.01, 0.01),
-                 omega_param = c(1.0, 50.0, 1.0, 5.0, 1.0),
-                 wishdf_param = c(15),
-                 prec_logy_hyp_param = c(4.0, 1.0))
   prep_priors(max_antigens = 2, 
-              priors = priors) |>
+              mu_hyp_param = c(1.0,  5.0, 0.0, -2.0, -3.0),
+              prec_hyp_param = c(0.01, 0.01, 0.01, 0.01, 0.01),
+              omega_param = c(1.0, 50.0, 1.0, 5.0, 1.0),
+              wishdf_param = c(15),
+              prec_logy_hyp_param = c(4.0, 1.0)) |>
     expect_snapshot_value(style = "deparse")
 })
