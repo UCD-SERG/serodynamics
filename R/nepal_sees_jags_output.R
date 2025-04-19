2c00becbdf49ec1e3eea6b4f592939429e8da1fc
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-18 17:58:10 -0700

    Updating output documentation

--- R/nepal_sees_jags_output.R
+++ R/nepal_sees_jags_output.R
@@ -6,7 +6,7 @@
 #' using the [nepal_sees] example data set as input
 #' and stratifying by column `"bldculres"`, 
 #' which is the diagnosis type (typhoid or
-#' paratyphoid). Keeping only IDs "newperson", "sees_npl_1", "sees_npl_2".
+#' paratyphoid). Keeping only IDs `"newperson"`, `"sees_npl_1"`, `"sees_npl_2"`.
 #'
 #' @format ## `nepal_sees_jags_output`
 #' A [list] consisting of the following named elements:
@@ -15,7 +15,7 @@
 #'   posterior predictive distribution of the person-specific parameters for a
 #'   "new person" with no observed data (`Subject = "newperson"`) and posterior
 #'   distributions of the person-specific parameters for two arbitrarily-chosen
-#'   subjects (`"sees_npl_1"` and`"sees_npl_2"`)}
+#'   subjects (`"sees_npl_1"` and `"sees_npl_2"`)}
 #'  \item{attributes}{A [list] of `attributes` that summarize the jags inputs}
 #' }
 #' @source reference study: <https://doi.org/10.1016/S2666-5247(22)00114-8>
