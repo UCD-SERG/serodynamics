f4c59585ab8effd4d9b74807baae3977906bebd8
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-20 14:50:20 -0700

    Fixing documentation specifics

--- R/prep_priors.R
+++ R/prep_priors.R
@@ -8,7 +8,7 @@
 #' @param mu_hyp_param A [numeric] [vector] of 5 values representing the mean of
 #' the hyperprior distribution of the population mean of the random
 #' person-specific seroresponse curve parameters.
-#' If specified must be 5 values long representing the following parameters:
+#' If specified, must be 5 values long, representing the following parameters:
 #'    - y0 = baseline antibody concentration (default = 1.0)
 #'    - y1 = peak antibody concentration (default = 7.0)
 #'    - t1 = time to peak (default = 1.0)
@@ -16,16 +16,18 @@
 #'    - alpha = decay rate (default = -1.0)
 #' @param prec_hyp_param A [numeric] [vector] of 5 values corresponding to
 #' diagonal entries representing the precision matrix of the hyperprior
-#' distribution of the person-specific seroresponse curve parameters.
-#' If specified must be 5 values long:
+#' distributions of the population means of the seroresponse curve parameters
+#' (reused for each antigen-isotype).
+#' If specified, must be 5 values long:
 #'    - defaults: y0 = 1.0, y1 = 0.00001, t1 = 1.0, r = 0.001, alpha = 1.0
-#' @param omega_param A [numeric] [vector] of 5 values corresponding to diagonal
-#' entries representing the precision matrix of Wishart hyper-priors for
-#' person-specific random effects.
-#' If specified must be 5 values long:
+#' @param omega_param A [numeric] [vector] of 5 values corresponding to the
+#' diagonal entries of the (diagonal) scale matrix of the Wishart hyperprior
+#' distributions of the person-specific random effects' precision matrices.
+#' If specified, must be 5 values long:
 #'    - defaults: y0 = 1.0, y1 = 50.0, t1 = 1.0, r = 10.0, alpha = 1.0
-#' @param wishdf_param A [numeric] [vector] of 1 value specifying Wishart
-#' distribution degrees of freedom.
+#' @param wishdf_param An [integer] [vector] of 1 value specifying the degrees
+#' of freedom for the Wishart hyperprior distributions of the random effects'
+#' precision matrices.
 #' If specified, must be 1 value long.
 #'    - default = 20.0
 #' @param prec_logy_hyp_param A [numeric] [vector] of 2 values corresponding to
@@ -38,9 +40,11 @@
 #' - "mu.hyp": A [matrix] of hyperpriors with dimensions
 #' number of antigens x 5 (number of parameters), representing the mean of the
 #' hyperprior distribution: y0, y1, t1, r, and alpha).
-#' - "prec.hyp": A three-dimensional [numeric] [array] with 5 [matrix], each
-#' with dimensions number of antigens x 5 (# of parameters), representing
-#' precision matrix of the hyperprior distribution
+#' - "prec.hyp": A three-dimensional [numeric] [array] 
+#' with dimensions `max_antigens` x 5 (# of parameters) x 5 (# of parameters), 
+#' containing the precision matrices of the hyperprior distributions 
+#' on the population means of the seroresponse parameters, 
+#' for each antigen-isotype.
 #' - "omega" : A three-dimensional [numeric] [array] with 5 [matrix],each
 #' with dimensions number of antigens x 5 (# of parameters), representing the
 #' precision matrix of Wishart hyper-priors
