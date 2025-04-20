c670695c2ecce9e3461005e87343daba483a7bb4
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-20 15:14:26 -0700

    Updating test input

--- tests/testthat/test-prep_priors.R
+++ tests/testthat/test-prep_priors.R
@@ -9,7 +9,7 @@ test_that("priors are modifiable", {
               mu_hyp_param = c(1.0,  5.0, 0.0, -2.0, -3.0),
               prec_hyp_param = c(0.01, 0.01, 0.01, 0.01, 0.01),
               omega_param = c(1.0, 50.0, 1.0, 5.0, 1.0),
-              wishdf_param = c(15),
+              wishdf_param = 15,
               prec_logy_hyp_param = c(4.0, 1.0)) |>
     expect_snapshot_value(style = "deparse")
 })
