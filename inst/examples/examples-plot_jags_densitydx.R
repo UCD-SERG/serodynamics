d5932c9dad963f9710b6c3940615b339c0a5c265
sschildhauer <sschildhauer@ucdavis.edu>
2025-04-28 11:25:29 -0700

    Updating lint in example

--- inst/examples/examples-plot_jags_densitydx.R
+++ inst/examples/examples-plot_jags_densitydx.R
@@ -1,8 +1,8 @@
 
 
-  data <- serodynamics::nepal_sees_jags_output
+data <- serodynamics::nepal_sees_jags_output
 
-  plot_jags_dens(
-                 data = data,
-                 iso = "HlyE_IgA",
-                 strat = "typhi")
+plot_jags_dens(
+               data = data,
+               iso = "HlyE_IgA",
+               strat = "typhi")
