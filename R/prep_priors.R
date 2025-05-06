37d3b72cb2b08d075a3f1e59e6220a90a9a2dc75
sschildhauer <sschildhauer@ucdavis.edu>
2025-05-05 17:29:54 -0700

    Updating prior documentation

--- R/prep_priors.R
+++ R/prep_priors.R
@@ -5,9 +5,9 @@
 #' 
 #' @param max_antigens An [integer] specifying how many
 #' antigen-isotypes will be modeled
-#' @param mu_hyp_param A [numeric] [vector] of 5 values representing the mean of
-#' the hyperprior distribution of the population mean of the random
-#' person-specific seroresponse curve parameters.
+#' @param mu_hyp_param A [numeric] [vector] of 5 values representing the prior
+#' guess for the population mean of the 5 estimated seroresponse curve
+#' parameters (y0, y1, t1, r, alpha) for each biomarker.
 #' If specified, must be 5 values long, representing the following parameters:
 #'    - y0 = baseline antibody concentration (default = 1.0)
 #'    - y1 = peak antibody concentration (default = 7.0)
@@ -15,24 +15,22 @@
 #'    - r = shape parameter (default = -4.0)
 #'    - alpha = decay rate (default = -1.0)
 #' @param prec_hyp_param A [numeric] [vector] of 5 values corresponding to
-#' diagonal entries representing the precision matrix of the hyperprior
-#' distributions of the population means of the seroresponse curve parameters
-#' (reused for each antigen-isotype).
+#' hyperprior diagonal entries for the precision matrix (i.e. inverse variance)
+#' representing prior beliefs of uncertainty around `mu_hyp_param`.
 #' If specified, must be 5 values long:
 #'    - defaults: y0 = 1.0, y1 = 0.00001, t1 = 1.0, r = 0.001, alpha = 1.0
 #' @param omega_param A [numeric] [vector] of 5 values corresponding to the
-#' diagonal entries of the (diagonal) scale matrix of the Wishart hyperprior
-#' distributions of the person-specific random effects' precision matrices.
+#' diagonal entries representing the Wishart hyperprior
+#' distributions of `prec_hyp_param`.
 #' If specified, must be 5 values long:
 #'    - defaults: y0 = 1.0, y1 = 50.0, t1 = 1.0, r = 10.0, alpha = 1.0
 #' @param wishdf_param An [integer] [vector] of 1 value specifying the degrees
-#' of freedom for the Wishart hyperprior distributions of the random effects'
-#' precision matrices.
+#' of freedom for the Wishart hyperprior distribution of `prec_hyp_param`.
 #' If specified, must be 1 value long.
 #'    - default = 20.0
 #' @param prec_logy_hyp_param A [numeric] [vector] of 2 values corresponding to
-#' the hyper-parameters for the precision (inverse variance) of the biomarkers,
-#' on log-scale. If specified, must be 2 values long.
+#' hyperprior diagonal entries on the log-scale for the precision matrix
+#' (i.e. inverse variance) representing prior beliefs of individual variation.
 #'    - defaults = 4.0, 1.0
 #'
 #' @returns A "curve_params_priors" object 
@@ -40,21 +38,20 @@
 #' as an [attributes] entry named `"used_priors"`).
 #' - "n_params": Corresponds to the 5 parameters being estimated.
 #' - "mu.hyp": A [matrix] of hyperpriors with dimensions
-#' number of antigens x 5 (number of parameters), representing the mean of the
-#' hyperprior distribution: y0, y1, t1, r, and alpha).
+#' `max_antigens` x 5 (number of parameters), representing the mean of the
+#' hyperprior distribution for each biomarker: y0, y1, t1, r, and alpha).
 #' - "prec.hyp": A three-dimensional [numeric] [array] 
-#' with dimensions `max_antigens` x 5 (# of parameters) x 5 (# of parameters), 
-#' containing the precision matrices of the hyperprior distributions 
-#' on the population means of the seroresponse parameters, 
-#' for each antigen-isotype.
+#' with dimensions `max_antigens` x 5 (# of parameters), 
+#' containing the precision matrices of the hyperprior distributions of
+#' `mu_hyp_param`,for each biomarker.
 #' - "omega" : A three-dimensional [numeric] [array] with 5 [matrix],each
-#' with dimensions number of antigens x 5 (# of parameters), representing the
-#' precision matrix of Wishart hyper-priors
+#' with dimensions `max_antigens` x 5 (# of parameters), representing the
+#' precision matrix of Wishart hyper-priors for `prec_hyp_param`
 #' - "wishdf": A [vector] of 2 values specifying Wishart distribution degrees
-#' of freedom.
+#' of freedom for `prec_hyp_param`.
 #' - "prec.logy.hyp": A [matrix] of hyper-parameters for the precision
-#' (inverse variance) of the biomarkers, on log-scale, measuring
-#' number of antigens x 2.
+#' (inverse variance) of individual variation measuring
+#' `max_antigens` x 2., on the log-scale, 
 #' @export
 #' @examples
 #' prep_priors(max_antigens = 2)
