0ac6a3edfbd8f063e35db49f1518d95c0276d5a6
sschildhauer <sschildhauer@ucdavis.edu>
2025-06-24 17:01:25 -0700

    Updating calc_fit_mod to work within the run_mod function

--- R/calc_fit_mod.R
+++ R/calc_fit_mod.R
@@ -1,34 +1,32 @@
-#' @title Calculates fitted values for run_mod output
+#' @title Calculates fitted and residual values for modeled outputs
 #' @description
-#'  `calc_fit_mod()` takes antibody kinetic parameters and creates a fitted
-#'  value corresponding to the estimated assay value (ex. ELISA units etc.) at
-#'  time since infection (TSI).
-#' @param t A [vector] of time points used to plot the fitted values.
-#' @returns A [vector] of fitted
-#'  value corresponding to the estimated assay value (ex. ELISA units etc.) at
-#'  time since infection (TSI).
-#' @export
-#' @example inst/examples/run_mod-examples.R
+#'  `calc_fit_mod()` takes antibody kinetic parameter estimates and calculates
+#'  fitted and residual values. Fitted values correspond to the estimated assay
+#'  value (ex. ELISA units etc.) at time since infection (TSI). Residual values
+#'  are calculate as the difference between fitted and observed values.
+#' @param input_dat A [] dataframe of modeled antibody kinetic parameter values.
+#' @param original_data A [] data frame of the original input dataset.
+#' @returns A [data.frame] attached as an [attributes] with the following
+#' values:
+#'   - Subject = ID number specifying an individual
+#'   - Iso_type = The modeled antigen_isotype
+#'   - t = Time since infection 
+#'   - fitted = The fitted value calculated using model output parameters for a
+#'   given `t`
+#'   - residual = The residual value calculated as the difference between
+#'   observed and fitted values for a given `t`
 
 calc_fit_mod <- function(input_dat = jags_out, 
-                         org_data = dl_sub) {
-  # Matching time_since_infection from original data set.
+                         original_data = orig_data) {
+  # Preparing modeled data
   input_dat <- input_dat |>
     dplyr::group_by(.data$Parameter, .data$Iso_type, .data$Stratification, 
                     .data$Subject) |>
     dplyr::summarize(med_value = median(.data$value)) |>
     tidyr::spread(.data$Parameter, .data$med_value)
 
-  # Preparing input data to match time
-  org_data <- dl_sub |>
-    rename(Subject = attributes(dl_sub)$id_var,
-           Iso_type = attributes(dl_sub)$biomarker_var,
-           t = attributes(dl_sub)$timeindays,
-           result = attributes(dl_sub)$value_var) |>
-    select(.data$Subject, .data$Iso_type, .data$t, .data$result)
-
   # Matching input data with modeled data
-  matched_dat <- merge(input_dat, org_data, by = c("Subject", "Iso_type"),
+  matched_dat <- merge(input_dat, original_data, by = c("Subject", "Iso_type"),
                        all.y = TRUE)
 
   # Calculating fitted and residual
@@ -38,7 +36,7 @@ calc_fit_mod <- function(input_dat = jags_out,
                                           * .data$t),
                            (.data$y1 ^ (1 - shape) - (1 - .data$shape) *
                               .data$alpha * (.data$t - .data$t1)) ^ 
-                             (1 / (1 - .data$shape)),
-                           residual = .data$result - .data$fitted) |>
+                             (1 / (1 - .data$shape))),
+           residual = .data$result - .data$fitted) |>
     select(.data$Subject, .data$Iso_type, .data$t, .data$fitted, .data$residual)
-    }
+}
