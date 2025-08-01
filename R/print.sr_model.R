e6b54c1c57dcc402d6bdd16f4df8e78c56506f15
sschildhauer <sschildhauer@ucdavis.edu>
2025-08-01 11:49:09 +0700

    Fixing for closure

--- R/print.sr_model.R
+++ R/print.sr_model.R
@@ -25,7 +25,7 @@ print.sr_model <- function(x, ...) { # nolint
                        values_from = .data$mean_val) |>
     dplyr::arrange(.data$Iso_type)
   # Taking out stratification column if not specified
-  if (unique(data$Stratification == "None")) {
+  if (unique(.data$Stratification == "None")) {
     x <- x |> select(-all_of(.data$Stratification))
   } 
   print(data.frame(x))
