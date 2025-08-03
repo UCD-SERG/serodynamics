49af0008960b4643cccc8b6d953fb9c6a74943a6
sschildhauer <sschildhauer@ucdavis.edu>
2025-08-03 20:02:17 +0700

    Creating use_att_names function

---
+++ R/use_att_names.R
@@ -0,0 +1,19 @@
+#' @title Assigns column attribute name directly to column
+#' @description
+#'  `use_att_names` takes prepared longitudinal data for antibody kinetic
+#'  modeling and names columns based off of attributes to allow mergine
+#'  with a modeled [run_mod()] output [dplyr::tbl_df]. The column names include
+#'  `Subject`, `Iso_type`, `t`, and `result`. 
+#' @param data A [data.frame] raw longitudinal data that has been
+#' prepared for antibody kinetic modeling using [as_case_data()].
+#' @returns The input [data.frame] with columns named after attributes.
+#' @keywords internal
+use_att_names <- function(data) {
+  data <- data |> 
+    dplyr::rename(
+      Subject = data |> serocalculator::ids_varname(),
+      Iso_type = data |> serocalculator::get_biomarker_names_var(),
+      t = data |> get_timeindays_var(),
+      result = data |> serocalculator::get_values_var()
+    ) 
+}
