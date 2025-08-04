4233b52596183e23806d4724598d56729119cece
sschildhauer <sschildhauer@ucdavis.edu>
2025-08-04 16:14:42 +0700

    Updating snap

--- tests/testthat/test-print.sr_model.R
+++ tests/testthat/test-print.sr_model.R
@@ -1,6 +1,8 @@
 test_that(
   desc = "results are consistent with printed output for sr_model class",
   code = {
-    expect_snapshot(nepal_sees_jags_output)
+    nepal_sees_jags_output |>
+      suppressWarnings() |>
+      expect_snapshot() 
   }
 )
