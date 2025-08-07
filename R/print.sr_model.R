37f451e33ccc6fe940814aa6d56e5e5e9c0d7fe4
sschildhauer <sschildhauer@ucdavis.edu>
2025-08-07 13:10:20 +0700

    Making model output median value

--- R/print.sr_model.R
+++ R/print.sr_model.R
@@ -1,9 +1,9 @@
 #' @title Default print for [serodynamics::run_mod()] output object of class
 #' `sr_model`
 #' @description
-#'  A default print method for class `sr_model` that includes the mean posterior
-#'  distribution for antibody kinetic curve parameters by `Iso_type` and
-#'  `Stratification` (if specified).
+#'  A default print method for class `sr_model` that includes the median
+#'  posterior distribution for antibody kinetic curve parameters by `Iso_type`
+#'  and `Stratification` (if specified).
 #' @param x An `sr_model` output object from [run_mod()].
 #' @param ... Additional arguments affecting the summary produced.
 #' [serodynamics::run_mod()] function.
@@ -15,13 +15,13 @@
 #' print(nepal_sees_jags_output)
 print.sr_model <- function(x, ...) { # nolint
   
-  cat("An sr_model with the following mean values:")
+  cat("An sr_model with the following median values:")
   cat("\n")
   cat("\n")
   x <- x |>
     dplyr::summarise(.by = c(.data$Stratification, .data$Iso_type, 
                              .data$Parameter), 
-                     mean_val = mean(.data$value)) |>
+                     median_val = stats::median(.data$value)) |>
     tidyr::pivot_wider(names_from = .data$Parameter, 
                        values_from = .data$mean_val) |>
     dplyr::arrange(.data$Iso_type)
