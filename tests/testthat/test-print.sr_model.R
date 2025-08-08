9f2fd61a1b038df496865eff79455458bd91e41d
sschildhauer <sschildhauer@ucdavis.edu>
2025-08-08 15:14:49 +0700

    Fixing lint

--- tests/testthat/test-print.sr_model.R
+++ tests/testthat/test-print.sr_model.R
@@ -1,6 +1,6 @@
 test_that(
   desc = "results are consistent with printed output for sr_model class",
   code = {
-      testthat::expect_snapshot(nepal_sees_jags_output) 
+    testthat::expect_snapshot(nepal_sees_jags_output) 
   }
 )
