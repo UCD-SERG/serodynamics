87dd5c7351870aa0b4bcf0f406ed819078bdfdb2
sschildhauer <sschildhauer@ucdavis.edu>
2025-08-08 14:49:45 +0700

    troubleshooting snap

--- tests/testthat/test-print.sr_model.R
+++ tests/testthat/test-print.sr_model.R
@@ -1,8 +1,6 @@
 test_that(
   desc = "results are consistent with printed output for sr_model class",
   code = {
-    nepal_sees_jags_output |>
-      suppressWarnings() |>
-      expect_snapshot() 
+      testthat::expect_snapshot(nepal_sees_jags_output) 
   }
 )
