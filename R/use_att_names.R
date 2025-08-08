77b14096744c2b8001b00844921ce0437c6d7ee5
sschildhauer <sschildhauer@ucdavis.edu>
2025-08-08 15:51:57 +0700

    Updating type-o in documentation

--- R/use_att_names.R
+++ R/use_att_names.R
@@ -1,7 +1,7 @@
 #' @title Assigns column attribute name directly to column
 #' @description
 #'  `use_att_names` takes prepared longitudinal data for antibody kinetic
-#'  modeling and names columns based off of attributes to allow mergine
+#'  modeling and names columns based off of attributes to allow merging
 #'  with a modeled [run_mod()] output [dplyr::tbl_df]. The column names include
 #'  `Subject`, `Iso_type`, `t`, and `result`. 
 #' @param data A [data.frame] raw longitudinal data that has been
