fda2c2435c2df0730bbcc227ce49a4c8940db341
sschildhauer <sschildhauer@ucdavis.edu>
2025-09-28 15:39:57 -0700

    Updating Rhat test

--- tests/testthat/test-plot_jags_rhatdx.R
+++ tests/testthat/test-plot_jags_rhatdx.R
@@ -3,13 +3,13 @@ test_that(
   desc = "results are consistent with ggplot rhat dotplot output",
   code = {
     skip_if(getRversion() < "4.4.1") # 4.3.3 had issues
-
+    
     data <- serodynamics::nepal_sees_jags_output |>
       suppressWarnings()
-
+    
     # Testing for any errors:
     results <- plot_jags_Rhat(data) |> expect_no_error()
-      
+    
     # Test to ensure output is a list object:
     expect_true(is.list(results))
     
