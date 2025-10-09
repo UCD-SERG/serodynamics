eb7c83ae620047ee5d0f57cf96504e7f027d9acd
sschildhauer <sschildhauer@ucdavis.edu>
2025-10-08 18:01:21 -0700

    Updating documentation of add_jags_atts

--- R/add_jags_atts.R
+++ R/add_jags_atts.R
@@ -3,7 +3,7 @@
 #'  `add_jags_attrs` adds specified attributes to a [data.frame].
 #' @param df A [data.frame].
 #' @param attrs [attributes] to attach to the [data.frame].
-#' @param original_data A [data.frame] of the original input dataset.
+#' @param df A [data.frame] of the original input dataset.
 #' @returns A [data.frame] with specified [attributes] attached.
 #' @keywords internal
 add_jags_attrs <- function(df, attrs) {
