f0bf0680ba5e28a5ecbf0ce758f5c12c4345c939
sschildhauer <sschildhauer@ucdavis.edu>
2025-10-08 18:32:18 -0700

    Updating tests for print

--- tests/testthat/test-print.sr_model.R
+++ tests/testthat/test-print.sr_model.R
@@ -1,6 +1,18 @@
 test_that(
   desc = "results are consistent with printed output for sr_model class",
   code = {
-    testthat::expect_snapshot(nepal_sees_jags_output) 
+    results <- print(nepal_sees_jags_output)
+    results |>
+    testthat::expect_snapshot("as summary") 
+  }
+)
+
+test_that(
+  desc = "results are consistent with printed output for sr_model class 
+  as a tbl",
+  code = {
+    results <- print(nepal_sees_jags_output, print_tbl = TRUE)
+    results |>
+    testthat::expect_snapshot_data("as tbl") 
   }
 )
