f3f19ec699c52854fc183a3745d4546bf9d6b71d
sschildhauer <sschildhauer@ucdavis.edu>
2025-12-29 19:45:48 -0800

    Updating lint

--- R/Run_Mod.R
+++ R/Run_Mod.R
@@ -171,8 +171,8 @@ run_mod <- function(data,
       dplyr::rename(c("Iso_type" = "attributes.longdata..antigens",
                       "Subject_mcmc" = "attr.longdata...ids..")) |>
       mutate(Subject_mcmc = ifelse(is.na(.data$Subject_mcmc), 
-                                            .data$Subject, 
-                                            .data$Subject_mcmc)) |>
+                                   .data$Subject, 
+                                   .data$Subject_mcmc)) |>
       dplyr::select(!c("Subnum", "Subject", "Parameter")) |>
       dplyr::rename(c("Subject" = "Subject_mcmc",
                       "Parameter" = "Param"))
