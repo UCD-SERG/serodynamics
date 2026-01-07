ebf52f2753956b928e0686266a88d1cf5e252d95
sschildhauer <sschildhauer@ucdavis.edu>
2026-01-07 11:09:21 -0800

    Finalizing population parameter functions

--- R/prep_popparams.R
+++ R/prep_popparams.R
@@ -1,12 +1,13 @@
 #' @title Preparing population parameters
 #' @author Sam Schildhauer
 #' @description
-#'  `param_recode` recodes character numbers as their corresponding parameter.
+#'  `prep_popparams` filters a [data.frame] to only include population 
+#'  parameters and renames the `Subject` variable as `Population_Parameters`.
 #' @param x A [data.frame] with a `Subject` variable.
 #' @returns A filtered [data.frame] with rename `Subject` variable.
 #' @keywords internal
   prep_popparams <- function(x) { 
-    x |>
+    x <- x |>
     dplyr::filter(.data$Subject %in% c("mu.par", "prec.par", "prec.logy")) |>
     dplyr::rename(Population_Parameter = .data$Subject)
     return(x)
