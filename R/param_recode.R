f2a6b12d66ed73785189f7c16c24aed1b079eae3
sschildhauer <sschildhauer@ucdavis.edu>
2026-01-07 11:33:10 -0800

    Fixing lint in param_recode

--- R/param_recode.R
+++ R/param_recode.R
@@ -5,7 +5,7 @@
 #' @param x A [vector] of character numbers that represent parameters.
 #' @returns A [vector] with recoded values.
 #' @keywords internal
-  param_recode <- function(x) {
-    dplyr::recode(x, "1" = "y0", "2" = "y1", "3" = "t1", "4" = "alpha", 
-                  "5" = "shape")
-  }
+param_recode <- function(x) {
+  dplyr::recode(x, "1" = "y0", "2" = "y1", "3" = "t1", "4" = "alpha", 
+                "5" = "shape")
+}
