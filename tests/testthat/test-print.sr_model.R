a7ddcc043417609cff1e4b4bf1b996a3f4078b36
sschildhauer <sschildhauer@ucdavis.edu>
2026-04-08 14:15:06 +0700

    Fixing lint

--- tests/testthat/test-print.sr_model.R
+++ tests/testthat/test-print.sr_model.R
@@ -16,18 +16,18 @@ test_that(
   desc = "results are consistent with printed output for sr_model class as tbl
   with no stratification",
   code = {
-dataset <- serodynamics::nepal_sees 
-results <- run_mod(
-  data = dataset, # The data set input
-  file_mod = serodynamics_example("model.jags"),
-  nchain = 2, # Number of mcmc chains to run
-  nadapt = 10, # Number of adaptations to run
-  nburn = 10, # Number of unrecorded samples before sampling begins
-  nmc = 100,
-  niter = 100, # Number of iterations
-) |>
-  suppressWarnings()
+    dataset <- serodynamics::nepal_sees
+    results <- run_mod(
+      data = dataset, # The data set input
+      file_mod = serodynamics_example("model.jags"),
+      nchain = 2, # Number of mcmc chains to run
+      nadapt = 10, # Number of adaptations to run
+      nburn = 10, # Number of unrecorded samples before sampling begins
+      nmc = 100,
+      niter = 100, # Number of iterations
+    ) |>
+      suppressWarnings()
 
-testthat::expect_snapshot(print(results)) 
+    testthat::expect_snapshot(print(results)) 
   }
 )
