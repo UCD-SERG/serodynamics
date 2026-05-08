18fa7e887c131326f82cfbbf03346ec10de3970a
sschildhauer <sschildhauer@ucdavis.edu>
2026-05-08 13:46:15 -0700

    Fixing lint in prep_popparams

--- R/prep_popparams.R
+++ R/prep_popparams.R
@@ -10,6 +10,6 @@
 prep_popparams <- function(x) { 
   x <- x |>
     dplyr::filter(.data$Subject %in% c("mu.par", "prec.par", "prec.logy")) |>
-    dplyr::rename(Population_Parameter = Subject)
+    dplyr::rename(Population_Parameter = .data$Subject)
   return(x)
 } 
