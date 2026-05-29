b88afeead94e502562d94ca34acc5f3b879d761c
sschildhauer <sschildhauer@ucdavis.edu>
2026-05-29 14:26:40 -0700

    Merging with main

--- R/prep_popparams.R
+++ R/prep_popparams.R
@@ -3,13 +3,14 @@
 #' @description
 #'  `prep_popparams` filters a [data.frame] to only include population 
 #'  parameters and renames the `Subject` variable as `Population_Parameter`.
-#' @param x A [data.frame] with a `Subject` variable.
+#' @param x A [data.frame] with `Subject` and `.is_population_parameter`
+#'  variables.
 #' @returns A filtered [data.frame] with the `Subject` variable renamed to
 #' `Population_Parameter`.
 #' @keywords internal
-prep_popparams <- function(x) { 
-  x <- x |>
-    dplyr::filter(.data$Subject %in% c("mu.par", "prec.par", "prec.logy")) |>
-    dplyr::rename(Population_Parameter = "Subject")
-  return(x)
-} 
+prep_popparams <- function(x) {
+  x |>
+    dplyr::filter(.data$.is_population_parameter) |>
+    dplyr::rename("Population_Parameter" = "Subject") |>
+    dplyr::select(-c(".is_population_parameter"))
+}
