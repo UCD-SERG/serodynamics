ab281b4a6333115dcd436a20079d8d5c50cbba4b
sschildhauer <sschildhauer@ucdavis.edu>
2026-06-09 16:58:35 -0700

    Modifying run_serodynamics to reference run_mod

--- R/Run_Mod.R
+++ R/Run_Mod.R
@@ -3,11 +3,13 @@
 #'  `r lifecycle::badge("deprecated")`
 #'  `run_mod()` was renamed to `run_serodynamics()` to create a more 
 #'  descriptive function name.
+#' @param ... Arguments passed to `run_serodynamics()`.
+#' @seealso run_serodynamics()
 #' @keywords internal
 #' @export
 run_mod <- function(...) {
   lifecycle::deprecate_warn(
-    when = "1.0.0",
+    when = "0.1.0",
     what = "run_mod()",
     with = "run_serodynamics()"
   )
