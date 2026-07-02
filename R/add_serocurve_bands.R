9f40d47ba866cdeb64b72d3c0ec5ca2fb8890ae8
sschildhauer <sschildhauer@ucdavis.edu>
2026-07-02 15:09:49 -0700

    Fixing issue with labeling on stratified plots

--- R/add_serocurve_bands.R
+++ R/add_serocurve_bands.R
@@ -2,7 +2,48 @@
 # ribbon) to a `plot_serocurve()` plot, coloured/filled by Stratification
 # when `multi_strat` is TRUE and by a fixed "median"/"ci" legend entry
 # otherwise.
+# Adds the median line (and, if `show_ci`, the 95% credible interval
+# ribbon) to a `plot_serocurve()` plot.
 add_serocurve_bands <- function(p, curve_summary, show_ci, multi_strat) {
+  
+  if (multi_strat) {
+    
+    if (show_ci) {
+      p <- p +
+        ggplot2::geom_ribbon(
+          data = curve_summary,
+          ggplot2::aes(
+            x = .data$t,
+            ymin = .data$res_low,
+            ymax = .data$res_high,
+            fill = .data$Stratification
+          ),
+          alpha = 0.2,
+          inherit.aes = FALSE
+        )
+    }
+    
+    p <- p +
+      ggplot2::geom_line(
+        data = curve_summary,
+        ggplot2::aes(
+          x = .data$t,
+          y = .data$res_med,
+          colour = .data$Stratification
+        ),
+        linewidth = 1,
+        inherit.aes = FALSE
+      ) +
+      ggplot2::labs(
+        colour = "Stratification",
+        fill = "Stratification"
+      )
+    
+    return(p)
+  }
+  
+  ## ---------- Single-stratification plot ----------
+  
   if (show_ci) {
     p <- p +
       ggplot2::geom_ribbon(
@@ -11,53 +52,44 @@ add_serocurve_bands <- function(p, curve_summary, show_ci, multi_strat) {
           x = .data$t,
           ymin = .data$res_low,
           ymax = .data$res_high,
-          fill = if (multi_strat) .data$Stratification else "ci"
+          fill = "ci"
         ),
         alpha = 0.2,
         inherit.aes = FALSE
       )
   }
-
+  
   p <- p +
     ggplot2::geom_line(
       data = curve_summary,
       ggplot2::aes(
         x = .data$t,
         y = .data$res_med,
-        colour = if (multi_strat) .data$Stratification else "median"
+        colour = "median"
       ),
       linewidth = 1,
       inherit.aes = FALSE
-    )
-
-  if (multi_strat) {
-    return(p)
-  }
-
-  # ---- Legend for single-stratification plots ----------------------------
-  color_vals <- c("median" = "red")
-  color_labels <- c("median" = "Median")
-
-  p <- p +
-    ggplot2::scale_color_manual(
+    ) +
+    ggplot2::scale_colour_manual(
       name = "",
-      values = color_vals,
-      labels = color_labels,
-      guide = ggplot2::guide_legend(override.aes = list(shape = NA))
+      values = c(median = "red"),
+      labels = c(median = "Median"),
+      guide = ggplot2::guide_legend(
+        override.aes = list(shape = NA)
+      )
     )
-
+  
   if (show_ci) {
-    fill_vals <- c("ci" = "red")
-    fill_labels <- c("ci" = "95% credible interval")
-
     p <- p +
       ggplot2::scale_fill_manual(
         name = "",
-        values = fill_vals,
-        labels = fill_labels,
-        guide = ggplot2::guide_legend(override.aes = list(color = NA))
+        values = c(ci = "red"),
+        labels = c(ci = "95% credible interval"),
+        guide = ggplot2::guide_legend(
+          override.aes = list(colour = NA)
+        )
       )
   }
-
+  
   p
 }
