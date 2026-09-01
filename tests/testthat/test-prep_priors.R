72393414e7848e75ce35c5269d67d83f93d7470a
sschildhauer <sschildhauer@ucdavis.edu>
2026-08-31 19:19:14 -0700

    Updating tests as recommended by codecov report

--- tests/testthat/test-prep_priors.R
+++ tests/testthat/test-prep_priors.R
@@ -20,6 +20,15 @@ test_that("Preparing priors for exponential", {
     expect_snapshot_value(style = "deparse")
 })
 
+test_that("Omit mu_hyp_param under power decay", {
+  prep_priors(max_antigens = 2, 
+              prec_hyp_param = c(0.01, 0.01, 0.01, 0.01),
+              omega_param = c(1.0, 50.0, 1.0, 5.0, 1.0),
+              wishdf_param = 15,
+              prec_logy_hyp_param = c(4.0, 1.0)) |>
+    expect_error("Need to specify 5 priors for `mu_hyp_param`")
+})
+
 test_that("Omit prec_hyp_param under power decay", {
   prep_priors(max_antigens = 2, 
               mu_hyp_param = c(1.0,  5.0, 0.0, -2.0, -1.0),
@@ -28,6 +37,16 @@ test_that("Omit prec_hyp_param under power decay", {
               prec_logy_hyp_param = c(4.0, 1.0)) |>
     expect_error("Need to specify 5 priors for `prec_hyp_param`")
 })
+
+test_that("Omit omega_param under power decay", {
+  prep_priors(max_antigens = 2, 
+              mu_hyp_param = c(1.0,  5.0, 0.0, -2.0, -1.0),
+              prec_hyp_param = c(0.01, 0.01, 0.01, 0.01),
+              wishdf_param = 15,
+              prec_logy_hyp_param = c(4.0, 1.0)) |>
+    expect_error("Need to specify 5 priors for `omega_param`")
+})
+
 
 test_that("Expect error for mu_hyp_param under exponential", {
   prep_priors(max_antigens = 2, 
