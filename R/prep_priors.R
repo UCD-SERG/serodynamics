d215c62c68f53848f64c7764dc1f944354fb6b53
sschildhauer <sschildhauer@ucdavis.edu>
2026-09-14 10:15:58 -0700

    Taking out any reference to as default

--- R/prep_priors.R
+++ R/prep_priors.R
@@ -20,13 +20,13 @@
 #' @param prec_hyp_param A [numeric] [vector] of 5 values corresponding to
 #' hyperprior diagonal entries for the precision matrix (i.e. inverse variance)
 #' representing prior covariance of uncertainty around `mu_hyp_param`.
-#' Must be 5 values long corresponding to the 5 estimated parameters as default
+#' Must be 5 values long corresponding to the 5 estimated parameters 
 #' (4 values when `decay_type = "exponential"`).
 #' @param omega_param A [numeric] [vector] of 5 values corresponding to the
 #' diagonal entries representing the Wishart hyperprior
 #' distributions of `prec_hyp_param`, describing how much we expect parameters
 #' to vary between individuals.
-#' Must be 5 values long corresponding to the 5 estimated parameters as default
+#' Must be 5 values long corresponding to the 5 estimated parameters 
 #' (4 values when `decay_type = "exponential"`).
 #' @param wishdf_param An [integer] [vector] of 1 value specifying the degrees
 #' of freedom for the Wishart hyperprior distribution of `prec_hyp_param`.
